eaa5debfd9c6aa61cc97fa481c810a50943455a8
rbolden <regis.bolden@cgifederal.com>
2015-05-14 15:11:15 -0400

    Added accordion for release display

--- drupal/ux/ux-patterns/widgets/features.php
+++ drupal/ux/ux-patterns/widgets/features.php
@@ -1,22 +1,34 @@
 <div class="portal-landing-selection clearfix" style="margin-top: 30px;">
     <div class="feature-container col-md-6 col-sm-5">
         <h2>Release Notes</h2>
-        <h4>Version 0.1 [to be collapsible]</h4>
-        <p>The initial version of the E-Enterprise Portal is available. Several features have been implemented as part for this release. Read below for details.</p>
-        <ul class="release-notes">
-            <li><h4>Log in with E-Enterprise bridge using CDX account</h4>
-                <div>
-                    <p>User can log in with existing CDX user account. Account management remains with CDX Web.</p>
+        <ul class="accordion">
+            <li>
+                <a class="accordion-title" href="#pane-1" title="Click to expand description" style="margin-top: -0.5em;">Version 0.2 (planning)</a>
+                <div class="accordion-pane" id="pane-1" style="display: none; overflow: hidden;">
+                    <p>Currently in the planning phase. Check back for more details.</p>
                 </div>
             </li>
-            <li><h4>Log in with E-Enterprise bridge using third-party account</h4>
-                <div>
-                    <p>User can log in with existing Yahoo or MSN user account. Account management remains with provider.</p>
-                </div>
-            </li>
-            <li><h4>OneEPA template updates</h4>
-                <div>
-                    <p>OneEPA theme has been applied to Drupal instance for the E-Enterprise platform.</p>
+            <li>
+                <a class="accordion-title active" href="#pane-2" title="Click to expand description" style="margin-top: -0.5em;">Version 0.1</a>
+                <div class="accordion-pane active" id="pane-2" style="display: block !important; overflow:hidden;">
+                    <p>The initial version of the E-Enterprise Portal is available. Several features have been implemented as part for this release. Read below for details.</p>
+                    <ul class="release-notes">
+                        <li><h4>Log in with E-Enterprise bridge using CDX account</h4>
+                            <div>
+                                <p>User can log in with existing CDX user account. Account management remains with CDX Web.</p>
+                            </div>
+                        </li>
+                        <li><h4>Log in with E-Enterprise bridge using third-party account</h4>
+                            <div>
+                                <p>User can log in with existing Yahoo user account. Account management remains with provider.</p>
+                            </div>
+                        </li>
+                        <li><h4>OneEPA template updates</h4>
+                            <div>
+                                <p>OneEPA theme has been applied to Drupal instance for the E-Enterprise platform.</p>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </li>
         </ul>
@@ -34,3 +46,8 @@
         </div>
     </div>
 </div>
+
+<script>
+
+
+</script>
