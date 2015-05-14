56fb4c282b57df099999e86388538c602421fbd7
rbolden <regis.bolden@cgifederal.com>
2015-05-14 01:55:41 -0400

    New widget to support release notes and authentication on landing page.

---
+++ drupal/ux/ux-patterns/widgets/features.php
@@ -0,0 +1,36 @@
+<div class="portal-landing-selection clearfix" style="margin-top: 30px;">
+    <div class="feature-container col-md-6 col-sm-5">
+        <h2>Release Notes</h2>
+        <h4>Version 0.1 [to be collapsible]</h4>
+        <p>The initial version of the E-Enterprise Portal is available. Several features have been implemented as part for this release. Read below for details.</p>
+        <ul class="release-notes">
+            <li><h4>Log in with E-Enterprise bridge using CDX account</h4>
+                <div>
+                    <p>User can log in with existing CDX user account. Account management remains with CDX Web.</p>
+                </div>
+            </li>
+            <li><h4>Log in with E-Enterprise bridge using third-party account</h4>
+                <div>
+                    <p>User can log in with existing Yahoo or MSN user account. Account management remains with provider.</p>
+                </div>
+            </li>
+            <li><h4>OneEPA template updates</h4>
+                <div>
+                    <p>OneEPA theme has been applied to Drupal instance for the E-Enterprise platform.</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="credential-selection col-md-6 col-sm-5">
+        <h2>Already have an account? Log in with</h2>
+        <div><input type="submit" id="edit-e-enterprise-bridge" name="op" value="User ID from existing Account" class="form-submit"><input type="hidden" name="form_build_id" value="form-n6ttQ7o5peYQAZ78uGxkzYrvdIgH-yAMfoHIW7Wox6E">
+            <input type="hidden" name="form_id" value="eenterprise_bridge_auth_login_form">
+        </div>
+
+        <p>Subscribe to learn first about the launch</p>
+        <div>
+            <input type="text" />
+            <input type="button" value="Subscribe" />
+        </div>
+    </div>
+</div>
