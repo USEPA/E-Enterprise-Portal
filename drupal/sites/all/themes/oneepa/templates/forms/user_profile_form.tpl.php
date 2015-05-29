4f5143bf33556b57e6d0691659705c05b81cc7ab
Kate Walser <kate@cxinsights.com>
2015-05-29 16:47:20 -0400

    EE-236: Sprint 2 style adjustments * Adds My Account (profile), Logout to mobile menu * Moves Login button up on landing page * Adds headings to profile page

--- drupal/sites/all/themes/oneepa/templates/forms/user_profile_form.tpl.php
+++ drupal/sites/all/themes/oneepa/templates/forms/user_profile_form.tpl.php
@@ -1,12 +1,17 @@
 <?php
+print "<div class=\"edit-user-profile\">";
+print "<h3>User</h3>";
 print render($form['field_profile_first_name']);
 print render($form['field_profile_last_name']);
 print render($form['account']['mail']);
 ?>
-    <label for="edit-field-zip-code">Locations of Interest</label>
+<h3>Locations of Interest</h3>
 <?php
 print render($form['field_zip_code']);
+print "<h3>Interests</h3>";
 print render($form['field_profile_interests']);
+print "<h3>Favorites</h3>";
 print render($form['field_profile_favourites']);
 print drupal_render_children($form);
+print "</div>";
 ?>
