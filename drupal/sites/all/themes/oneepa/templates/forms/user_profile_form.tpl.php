5779eda9b7a246c73deffea8889f9a8db544ee48
ZTEGEGN <zerihun.tegegn@cgifederal.com>
2015-06-03 17:19:08 -0400

    Change Zip code wording EE-236

--- drupal/sites/all/themes/oneepa/templates/forms/user_profile_form.tpl.php
+++ drupal/sites/all/themes/oneepa/templates/forms/user_profile_form.tpl.php
@@ -6,7 +6,7 @@ print render($form['field_profile_last_name']);
 print render($form['account']['mail']);
 ?>
 <h3>Locations of Interest</h3>
-<p>Add your favorite cities to see environmental maps and information relevant to those areas. We'll use the first location as your primary location.</p>
+<p>Add your locations of interest to see environmental maps and information relevant to those areas. We will use the first location as your primary.</p>
 <?php
 print render($form['field_zip_code']);
 print "<h3>Interests</h3>";
