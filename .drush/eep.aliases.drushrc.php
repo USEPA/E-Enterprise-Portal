928317f2127ac46f0975ab68ae3b5de6c40d8534
ZTEGEGN <zerihun.tegegn@cgifederal.com>
2015-06-17 23:43:42 -0400

    For Bamboo Build, rush alias script

---
+++ .drush/eep.aliases.drushrc.php
@@ -0,0 +1,13 @@
+<?php
+$dev_root = '/var/www/html/drupal';
+$aliases['dev'] = array(
+    'uri' => 'ec2-52-4-39-127.compute-1.amazonaws.com',
+    'root' => $dev_root,
+    'remote-user' => 'ztegegn',
+    'remote-host' => 'ec2-52-4-39-127.compute-1.amazonaws.com',
+    'path-aliases' => array(
+        '%dump-dir' => '/var/www/dumps',
+        '%files' => $dev_root . '/sites/default/files'
+     )
+);
+?>
