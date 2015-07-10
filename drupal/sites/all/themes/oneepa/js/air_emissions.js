dafd1138614fe79f05a35de191a0f6cb7bc467fa
Jeff Soosiah <jsoosiah@gmail.com>
2015-07-09 22:15:48 -0400

    added legend; made graph responsive

--- drupal/sites/all/themes/oneepa/js/air_emissions.js
+++ drupal/sites/all/themes/oneepa/js/air_emissions.js
@@ -58,6 +58,27 @@
     });
   }
 
+  function insertSubscripts(text) {
+
+    text.each(function() {
+      var el = d3.select(this);
+
+      var parts = el.text().match(/(.*)([0-9])(.*)/);
+
+      if (parts) {
+        el.text('');
+
+        for (var i = 1; i < parts.length; i++) {
+          var tspan = el.append('tspan').text(parts[i]);
+          if (i == 2) { // subscript
+            tspan.attr("baseline-shift", "sub");
+            tspan.style("font-size", "80%")
+          }
+        }
+      }
+    });
+  }
+
   var emissionsNumberFormat = d3.format(",.0f");
 
   var drawDonut = function(data, svg, width, height, totalWidth) {
@@ -65,7 +86,7 @@
     var radius = Math.min(width, height) / 2;
 
     var color = d3.scale.ordinal()
-        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+        .range(["#857e00", "#f69000", "#a55d8c"]);
 
     var totalEmissions = 0;
 
@@ -86,8 +107,10 @@
         .value(function(d) { return d.TotEmissCO2E; });
 
     var svg = d3.select("#air-emissions-chart").append("svg")
-        .attr("width", totalWidth)
-        .attr("height", height);
+        .attr("viewBox", "0 0 " + totalWidth + " " + height)
+        .attr("preserveAspectRatio", "xMidYMid")
+        .style("max-width", totalWidth + "px")
+        // .attr("height", height);
 
     var donut = svg.append("g")
       .attr("transform", "translate(" + totalWidth / 2 + "," + height / 2 + ")");
@@ -127,13 +150,23 @@
       return d.data.FacName; 
     }).call(wrap, totalWidth / 2 - radius);
 
-    labels.append("tspan")
-      .text(function(d) {
-        return emissionsNumberFormat(d.data.TotEmissCO2E) + ' metric tons CO2E';
-      })
+    var co2eLabel = labels.append("tspan")
       .attr("x", function(){ return d3.select(this.parentNode).attr("x") })
       .attr("dy", baseDy)
       .attr("class", "emissions-watch-donut-small-subtitle");
+
+    co2eLabel.append("tspan")
+      .text(function(d) {
+        return emissionsNumberFormat(d.data.TotEmissCO2E) + ' metric tons CO';
+      });
+
+    co2eLabel.append("tspan")
+      .attr("baseline-shift", "sub")
+      .style("font-size", "80%")
+      .text("2");
+
+    co2eLabel.append("tspan")
+      .text("e");
 
     labels.append("tspan")
       .text(function(d) {
@@ -160,7 +193,19 @@
       .attr("dy", baseDy)
       .attr("x", 0)
       .attr("class", "emissions-watch-donut-small-subtitle")
-      .text("CO2E");
+      .text("CO");
+
+    totalEmissionsText.append("tspan")
+          // .attr("x", 0)
+          .attr("baseline-shift", "sub")
+          .style("font-size", "64%")
+          .attr("class", "emissions-watch-donut-small-subtitle")
+          .text("2");
+
+    totalEmissionsText.append("tspan")
+          // .attr("x", 0)
+          .attr("class", "emissions-watch-donut-small-subtitle")
+          .text("e");
 
     var lowestLabel;
 
@@ -169,7 +214,8 @@
         lowestLabel = this;
     })
 
-    svg.attr("height", height + lowestLabel.clientHeight);
+    // svg.attr("height", height + lowestLabel.clientHeight);
+    svg.attr("viewBox", "0 0 " + totalWidth + " " + (height + lowestLabel.clientHeight))
   }
 
   var drawChart = function(data) {
@@ -178,6 +224,7 @@
     var maxCO2E = 0;
 
     var facIDMap = {};
+    var uniqueGases = {};
 
     data.Facilities.forEach(function(facility, i) {
 
@@ -189,27 +236,33 @@
       if (facility.TotEmissCO2E > maxCO2E)
         maxCO2E = facility.TotEmissCO2E;
 
+      if (facility.Pollutants) {
+        facility.Pollutants.forEach(function(pollutant) {
+          uniqueGases[pollutant.PollutantName] = 1;
+        });
+      }
+
       // TODO: handle missing pollutants case
       // if (facility.Pollutants.length == 0)
       //   facility.Pollutants.push({'PollutantName': 'All', AmtCO2E: facility.TotEmissCO2E});
 
       var y0 = 0;
+      // prepare bar sections
       facility.sections = facility.Pollutants.map(function(pollutant) { return {name: pollutant.PollutantName, y0: y0, y1: y0 += +pollutant.AmtCO2E}; });
     });
 
-    var colorMap = {
-      'CO2': 'red',
-      'N2O': 'blue',
-      'CH4': 'green',
-      'PFC': 'purple',
-      'HFC': 'orange',
-      'SF6': 'yellow',
-      'NF3': 'indigo',
-      'All': 'black'
+    var gasClassMap = {
+      'PFC': 'gas-pfc',
+      'HFC': 'gas-hfc',
+      'NF3': 'gas-nf3',
+      'SF6': 'gas-sf6',
+      'N2O': 'gas-n2o',
+      'CH4': 'gas-ch4',
+      'CO2': 'gas-co2'
     }
 
     // set chart sizes
-    var margin = {top: 20, right: 60, bottom: 100, left: 60},
+    var margin = {top: 0, right: 20, bottom: 100, left: 140},
         width = 650 - margin.left - margin.right,
         height = 300 - margin.top - margin.bottom;
 
@@ -252,15 +305,17 @@
 
     // append svg
     var svg = d3.select("#air-emissions-chart").append("svg")
-        .attr("width", totalWidth)
-        .attr("height", totalHeight);
+        .attr("viewBox", "0 0 " + totalWidth + " " + totalHeight)
+        .attr("preserveAspectRatio", "xMidYMid")
+        .style("max-width", totalWidth + "px")
+        // .attr("height", totalHeight);
 
     // append bar chart
     var graph = svg.append("g")
         .attr("transform", "translate(" + margin.left + "," + (margin.top) + ")");
 
     // y-axis
-    graph.append("g")
+    var yAxisGroup = graph.append("g")
         .attr("class", "y axis")
         .call(yAxis);
 
@@ -278,7 +333,7 @@
         .attr("width", x.rangeBand())
         .attr("y", function(d) { return y(d.y1); })
         .attr("height", function(d) { return y(d.y0) - y(d.y1); })
-        .style("fill", function(d) { return colorMap[d.name]; });
+        .attr("class", function(d) { return gasClassMap[d.name]; });
 
     // x-axis labels
     var xAxisLabels = graph.append("g")
@@ -290,14 +345,27 @@
         .call(wrap, x.rangeBand());
 
     // x-axis sublabels
-    xAxisLabels.append("tspan")
-      .text(function(d) {
-        return (emissionsNumberFormat(data.Facilities[facIDMap[d]].TotEmissCO2E)) + ' metric tons CO2E';
-      })
+    var co2eLabel = xAxisLabels.append("tspan")
+      // .text(function(d) {
+      //   return (emissionsNumberFormat(data.Facilities[facIDMap[d]].TotEmissCO2E)) + ' metric tons CO2e';
+      // })
       .attr("x", function(){ return d3.select(this.parentNode).attr("x") })
       .attr("dy", baseDy)
       .attr("text-anchor", "start")
       .attr("class", "emissions-watch-donut-small-subtitle");
+
+    co2eLabel.append("tspan")
+      .text(function(d) {
+        return emissionsNumberFormat(data.Facilities[facIDMap[d]].TotEmissCO2E) + ' metric tons CO';
+      });
+
+    co2eLabel.append("tspan")
+      .attr("baseline-shift", "sub")
+      .style("font-size", "80%")
+      .text("2");
+
+    co2eLabel.append("tspan")
+      .text("e");
 
     // x-axis sublabels
     xAxisLabels.append("tspan")
@@ -310,7 +378,7 @@
       .attr("class", "emissions-watch-donut-small-subtitle");
 
     // component labels above bars
-    xAxisLabels.append("tspan")
+    var tspans = xAxisLabels.append("tspan")
       .text(function(d) {
         return data.Facilities[facIDMap[d]].Pollutants.map(
           function(pollutant){ 
@@ -320,7 +388,10 @@
       .attr("y", function(d) { return -y(maxCO2E - data.Facilities[facIDMap[d]].TotEmissCO2E) - baseDy * data.Facilities[facIDMap[d]].Pollutants.length })
       .attr("text-anchor", "start")
       .attr("class", "emissions-watch-bar-chart-small-subtitle")
-      .call(insertLinebreaks);
+      .call(insertLinebreaks)
+      // .call(insertSubscripts)
+
+    tspans.selectAll("tspan").call(insertSubscripts);
     
     xAxisLabels.append("tspan")
       .text(function(d) {
@@ -333,13 +404,52 @@
       .attr("text-anchor", "end")
       .attr("class", "emissions-watch-bar-chart-small-subtitle")
       .call(insertLinebreaks);
+
+    // y axis label
+    yAxisGroup.append("text")
+      .text("Emissions")
+      .attr("transform", "translate(-140,0)")
+      .style("font-size", "80%")
+      .append("tspan")
+        .text("(metric tons CO2e)")
+        .attr("x", 0)
+        .attr("dy", baseDy)
+        .attr("class", "emissions-watch-bar-chart-small-subtitle")
+        .call(insertSubscripts)
+
+
+    // legend
+    var legend = yAxisGroup.append("g")
+      .attr("transform", "translate(-140, 50)")
+      .attr("class", "legend");
+
+    legend.append("text").text("Legend").attr("x", 0).attr("y", 0).style("fill", "black");
+
+    var offset = baseDy/2;
+
+    var legendItem = legend.selectAll("g")
+      .data(Object.keys(uniqueGases))
+      .enter().append("g")
+
+    legendItem.append("rect").attr("width", x.rangeBand() / 3)
+        .attr("x", 0)
+        .attr("y", function(d, i) { return offset + i *  baseDy; })
+        .attr("height", baseDy)
+        .attr("class", function(d) { return gasClassMap[d]; });
+
+    legendItem.append("text").text(function(d) { return d; })
+      .attr("x", 0)
+      .attr("y", function(d, i) { return offset + (i+1) *  baseDy - baseDy / 3; })
+      .attr("text-anchor", "middle")
+      .attr("transform", "translate("+ x.rangeBand() / 6 +",0)")
+      .call(insertSubscripts);
   }
 
   var draw = function() {
     var data = { "ZipCentLat83": 32.8, "ZipCentLong83": -96.8, "BuffRadiusMi": 10, "Facilities": [ 
       { "FacID": 1001398, "FacName": "Atmos Energy Corporation - Texas", "FacLat83": 32.925488, "FacLong83": -96.816137, "FacDistMi": 8.72, "TotEmissCO2E": 544204, "Pollutants": [ { "PollutantName": "CH4", "AmtCO2E": 543549 }, { "PollutantName": "CO2", "AmtCO2E": 655 } ] }, 
       { "FacID": 1006817, "FacName": "McCommas Bluff Landfill", "FacLat83": 32.67628, "FacLong83": -96.73391, "FacDistMi": 9.37, "TotEmissCO2E": 420239.25, "Pollutants": [ { "PollutantName": "CH4", "AmtCO2E": 420239.25 } ] },
-      { "FacID": 1003945, "FacName": "TEXAS INSTRUMENTS NORTH CAMPUS", "FacLat83": 32.93311, "FacLong83": -96.7537, "FacDistMi": 9.58, "TotEmissCO2E": 531866.507, "Pollutants": [ { "PollutantName": "PFC", "AmtCO2E": 399276.483 }, { "PollutantName": "HFC", "AmtCO2E": 8872.334 }, { "PollutantName": "NF3", "AmtCO2E": 31682.4 }, { "PollutantName": "SF6", "AmtCO2E": 29906.76 }, { "PollutantName": "N2O", "AmtCO2E": 12737.414 }, { "PollutantName": "N2O", "AmtCO2E": 27.416 }, { "PollutantName": "CH4", "AmtCO2E": 23 }, { "PollutantName": "CO2", "AmtCO2E": 49340.7 } ] }
+      { "FacID": 1003945, "FacName": "TEXAS INSTRUMENTS NORTH CAMPUS", "FacLat83": 32.93311, "FacLong83": -96.7537, "FacDistMi": 9.58, "TotEmissCO2E": 531866.507, "Pollutants": [ { "PollutantName": "PFC", "AmtCO2E": 399276.483 }, { "PollutantName": "HFC", "AmtCO2E": 8872.334 }, { "PollutantName": "NF3", "AmtCO2E": 31682.4 }, { "PollutantName": "SF6", "AmtCO2E": 29906.76 }, { "PollutantName": "N2O", "AmtCO2E": 12737.414 }, { "PollutantName": "CH4", "AmtCO2E": 23 }, { "PollutantName": "CO2", "AmtCO2E": 49340.7 } ] }
     ] };
     
     drawChart(data);
