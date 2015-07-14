adba56349c96bcfdc9795efaa184ac825cbf205b
Benjamin Matkin <benjamin.matkin@cgifederal.com>
2015-07-14 14:25:31 -0500

    EE-466 changed default zip to 27705

--- drupal/sites/all/themes/oneepa/templates/forms/user_profile_form.tpl.php
+++ drupal/sites/all/themes/oneepa/templates/forms/user_profile_form.tpl.php
@@ -23,8 +23,7 @@ print render($form['account']['mail']);
 <h3>Locations of Interest</h3>
 <p class="eenterprise-utility-form-item-description-p">
 	Add your locations of interest to see environmental information relevant to those areas. 
-	Please indicate your default location by clicking on the key icon. 
-	Until a location is specified, the default location is set to Houston, Texas. <br />	
+	Until a location is specified, the default location is set to Durham, North Carolina. <br />	 <br />	
 	<span class='zip_code_ajax_error'></span>
 </p>
 <div id='zipcode_description' class='form-group'>
