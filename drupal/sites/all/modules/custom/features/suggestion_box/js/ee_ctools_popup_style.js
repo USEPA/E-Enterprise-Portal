6a1ad2e3ec32306416b497f6d025b144bbb27a6c
Jeff Soosiah <jsoosiah@gmail.com>
2015-08-04 18:11:44 -0400

    remove debug statmeent

--- drupal/sites/all/modules/custom/features/suggestion_box/js/ee_ctools_popup_style.js
+++ drupal/sites/all/modules/custom/features/suggestion_box/js/ee_ctools_popup_style.js
@@ -13,14 +13,5 @@ Drupal.theme.prototype.ee_ctools_popup_theme = function () {
   html += '    </div>';
   html += '  </div>';
 
-  console.log('ctools',Drupal.CTools.Modal.currentSettings.closeImage);
-
-  // html += '<div class="ui-dialog ui-widget ui-widget-content ui-corner-all ui-front ui-draggable" tabindex="-1" role="dialog">';
-  // html += '  <div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix">';
-  // html += '    <span id="ui-id-4" class="ui-dialog-title">Nevada State Events Web Map</span>';
-  // html += '    <button class="ui-dialog-titlebar-close"></button></div><div id="mapiFrame" class="mapiFrame ui-dialog-content ui-widget-content" style="width: auto; min-height: 0px; max-height: none; height: 55px;">';
-  // html += '  </div>';
-  // html += '</div>';
-
   return html;
 }
