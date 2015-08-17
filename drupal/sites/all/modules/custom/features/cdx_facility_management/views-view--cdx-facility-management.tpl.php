1baa4ce89b601960a468ab2c4cc982fa5f7ace9f
Benjamin Matkin <benjamin.matkin@cgifederal.com>
2015-08-17 15:41:03 -0500

    EE-552 add widget with sizing into modal, throw away iframe

--- drupal/sites/all/modules/custom/features/cdx_facility_management/views-view--cdx-facility-management.tpl.php
+++ drupal/sites/all/modules/custom/features/cdx_facility_management/views-view--cdx-facility-management.tpl.php
@@ -51,19 +51,8 @@ drupal_add_css('https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awes
 <script>
     $.initFacilityManagementWidget({
         autoScroll: false,
-        repositionFancyBoxes: true,
         widgetDisplayType: "Edit My Facilities",
-        frsRetrieveWidgetData: 'https://dev.epacdx.net/FrsPhase2/FrsFacilitiesScripts/RetrieveWidgetData',
-        frsGetManagedFacilities: 'https://dev.epacdx.net/FrsPhase2/FrsFacilitiesScripts/GetFacilitiesByUserRole',
-        frsGetEditingFacility: 'https://dev.epacdx.net/FrsPhase2/FrsFacilitiesScripts/GetFrsFacility',
-        frsSearchUrl: 'https://dev.epacdx.net/FrsPhase2/FrsFacilitiesScripts/SearchResults',
-        frsGetCountiesByState: 'https://dev.epacdx.net/FrsPhase2/FrsFacilitiesScripts/GetCountiesByState',
-        frsRemoveUserRoleFacility: 'https://dev.epacdx.net/FrsPhase2/FrsFacilitiesScripts/RemoveUserRoleFacility',
-        frsQueueUrl: 'https://dev.epacdx.net/FrsPhase2/FrsFacilitiesScripts/QueuedFacilites',
-        frsUpdateUrl: 'https://dev.epacdx.net/FrsPhase2/FrsFacilitiesScripts/SaveFrsFacility',
-        frsPreProcessFacility: 'https://dev.epacdx.net/FrsPhase2/FrsFacilitiesScripts/PreProcessFacility',
-        frsValidateCoordinatesForAddress: 'https://dev.epacdx.net/FrsPhase2/FrsFacilitiesScripts/ValidateCoordinatesForAddress',
-        frsAddFacilities: 'https://dev.epacdx.net/FrsPhase2/FrsFacilitiesScripts/AddFacilityToRole',
+        baseServiceUrl : 'https://dev.epacdx.net/FrsPhase2',
         ImagesFolderPath: "https://dev.epacdx.net/FrsPhase2/content/v3/FRS%20Widget/images", //static
         userRoleId: 80172,
         isRegistration: false, //static
