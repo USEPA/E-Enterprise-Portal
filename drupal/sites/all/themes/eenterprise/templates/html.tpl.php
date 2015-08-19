188d557258410899b3c14eeff3a95ed6a5fc9e33
Kate Walser <kate@cxinsights.com>
2015-08-18 22:15:08 -0400

    EE-440:  Landing page revisions * Implements new theme for revised landing page

---
+++ drupal/sites/all/themes/eenterprise/templates/html.tpl.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for the basic html structure of a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728208
+ * Tesing for Bamboo build
+ */
+?><!DOCTYPE html>
+<!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes; ?>><![endif]-->
+<!--[if lte IE 6]><html class="lt-ie9 lt-ie8 lt-ie7" <?php print $html_attributes; ?>><![endif]-->
+<!--[if (IE 7)&(!IEMobile)]><html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?>><![endif]-->
+<!--[if IE 8]><html class="lt-ie9" <?php print $html_attributes; ?>><![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html <?php print $html_attributes . $rdf_namespaces; ?>><!--<![endif]-->
+
+<head>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  <!--googleoff: all-->
+  <link href='//fonts.googleapis.com/css?family=Lato:300,400,700,900,400italic,700italic' rel='stylesheet' type='text/css'>
+  <?php print $styles; ?>
+  <?php //print $web_area_feed; ?>
+  <?php if ($add_html5_shim && !$add_respond_js): ?>
+    <!--[if lt IE 9]>
+    <script src="<?php print $base_path . $path_to_zen; ?>/js/html5.js"></script>
+    <![endif]-->
+  <?php elseif ($add_html5_shim && $add_respond_js): ?>
+    <!--[if lt IE 9]>
+    <script src="<?php print $base_path . $path_to_zen; ?>/js/html5-respond.js"></script>
+    <![endif]-->
+  <?php elseif ($add_respond_js): ?>
+    <!--[if lt IE 9]>
+    <script src="<?php print $base_path . $path_to_zen; ?>/js/respond.js"></script>
+    <![endif]-->
+  <?php endif; ?>
+  <?php print $scripts; ?>
+</head>
+<body class="<?php print $classes; ?> <?php
+if(!drupal_is_front_page()) {
+    print "wide-template";
+}
+?>" <?php print $attributes;?>>
+  <div class="skip-links"><a href="#main-content" class="skip-link element-invisible element-focusable"><?php print t('Jump to main content'); ?></a></div>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+  <?php //print $selectivizr; ?>
+  <?php //print $ie_scripts; ?>
+</body>
+</html>
