52bb6b340ac9ac4f4af88201f7456a78e0b61a61
rbolden <regis.bolden@cgifederal.com>
2015-08-21 18:43:38 -0400

    Minor modal updates for label/content display

--- drupal/sites/all/themes/eenterprise/templates/views/views-view-field--field-todo-lst-item.tpl.php
+++ drupal/sites/all/themes/eenterprise/templates/views/views-view-field--field-todo-lst-item.tpl.php
@@ -36,9 +36,9 @@ if(!empty($view->result[$view->row_index]->field_field_todo_lst_facility_name))
 if(!empty($view->result[$view->row_index]->field_field_todo_lst_part_code)) {
     $unique_id = $view->query->pager->current_page.'-'.$view->row_index;
     print "<div id='modal-page-details-todo-" . $unique_id . "' class='modal-content-in-page'>
-             <p><span class='modal-label'>Facility Information</span><br/> ".$view->result[$view->row_index]->field_field_todo_lst_facility_name[0]['rendered']['#markup']."(".$view->result[$view->row_index]->field_field_todo_lst_facility_reg_id[0]['rendered']['#markup'].")</p>
-             <p><span class='modal-label'>Part Information</span><br/> ".$view->result[$view->row_index]->field_field_todo_lst_part_code[0]['rendered']['#markup']." - ".$view->result[$view->row_index]->field_field_todo_lst_part_name[0]['rendered']['#markup']."</p>
-             <p><span class='modal-label'>Subpart Information</span><br/> ".$view->result[$view->row_index]->field_field_todo_lst_sub_part_code[0]['rendered']['#markup']." - ".$view->result[$view->row_index]->field_field_todo_lst_sub_part_name[0]['rendered']['#markup']."</p>
+             <p><span class='modal-label'>Facility Information</span><br/> ".$view->result[$view->row_index]->field_field_todo_lst_facility_name[0]['rendered']['#markup']." (".$view->result[$view->row_index]->field_field_todo_lst_facility_reg_id[0]['rendered']['#markup'].")</p>
+             <p><span class='modal-label'>Due Date</span><br/><strong> ".$view->result[$view->row_index]->field_field_todo_lst_due[0]['rendered']['#markup']."</strong></p>
+             <p><span class='modal-label'>Other Information</span><br/> ".$view->result[$view->row_index]->field_field_todo_lst_part_code[0]['rendered']['#markup']." - ".$view->result[$view->row_index]->field_field_todo_lst_part_name[0]['rendered']['#markup']."<br/> ".$view->result[$view->row_index]->field_field_todo_lst_sub_part_code[0]['rendered']['#markup']." - ".$view->result[$view->row_index]->field_field_todo_lst_sub_part_name[0]['rendered']['#markup']."</p>
              </div>";
     print "<br><a href='.' class='simple-dialog' id='to-do-details-link-" . $unique_id . "' rel='width:900;resizable:false;position:[center,center]' name='modal-page-details-todo-" . $unique_id . "' title='Item Details'>Details</a>";
 }
--- drupal/sites/all/themes/greentrees/templates/views/views-view-field--field-prog-tracker-app.tpl.php
+++ drupal/sites/all/themes/greentrees/templates/views/views-view-field--field-prog-tracker-app.tpl.php
@@ -31,18 +31,18 @@ if(!empty($view->result[$view->row_index]->field_field_prog_track_facility_name)
     //print "<div id='modal-page-details-" . $view->row_index . "' class='modal-content-in-page'>".$view->result[$view->row_index]->field_field_prog_track_item_details[0]['rendered']['#markup']."</div>";
     $unique_id = $view->query->pager->current_page.'-'.$view->row_index;
     print "<div id='modal-page-details-prog-track-" . $unique_id . "' class='modal-content-in-page'>
-            <p><b>Reporting Facility</b><br/> ".$view->result[$view->row_index]->field_field_prog_track_facility_name[0]['rendered']['#markup']." (".$view->result[$view->row_index]->field_field_prog_track_facility_reg_id[0]['rendered']['#markup'].")</p>
-            <p><b>Report will be published on</b><br/> " . $view->result[$view->row_index]->field_field_prog_track_updated[0]['rendered']['#markup']."</p>
-            <p><b>Other Information</b><br/> ".$view->result[$view->row_index]->field_field_prog_track_part_code[0]['rendered']['#markup']." - ".$view->result[$view->row_index]->field_field_prog_track_part_name[0]['rendered']['#markup']."<br/>".$view->result[$view->row_index]->field_field_prog_track_sub_part_code[0]['rendered']['#markup']." - ".$view->result[$view->row_index]->field_field_prog_track_sub_part_name[0]['rendered']['#markup']."</div>";
+            <p><span class='modal-label'>Reporting Facility</span><br/> ".$view->result[$view->row_index]->field_field_prog_track_facility_name[0]['rendered']['#markup']." (".$view->result[$view->row_index]->field_field_prog_track_facility_reg_id[0]['rendered']['#markup'].")</p>
+            <p><span class='modal-label'>Report will be published on</span><br/><strong> " . $view->result[$view->row_index]->field_field_prog_track_updated[0]['rendered']['#markup']."</strong></p>
+            <p><span class='modal-label'>Other Information</span><br/> ".$view->result[$view->row_index]->field_field_prog_track_part_code[0]['rendered']['#markup']." - ".$view->result[$view->row_index]->field_field_prog_track_part_name[0]['rendered']['#markup']."<br/>".$view->result[$view->row_index]->field_field_prog_track_sub_part_code[0]['rendered']['#markup']." - ".$view->result[$view->row_index]->field_field_prog_track_sub_part_name[0]['rendered']['#markup']."</div>";
     print "<br><a href='.' class='simple-dialog' id='details-link-" . $unique_id . "' rel='width:900;resizable:false;position:[center,center]' name='modal-page-details-prog-track-" . $unique_id . "' title='Item Details'>Details</a>";
 }
 else if(!empty($view->result[$view->row_index]->field_field_prog_track_status_note)) {
     $unique_id = $view->query->pager->current_page.'-'.$view->row_index;
     print "<div id='modal-page-details-prog-track-" . $unique_id . "' class='modal-content-in-page'>
-            <p><b>Application Information</b><br/>" . $view->result[$view->row_index]->field_field_prog_tracker_app[0]['rendered']['#markup']."</p>
-            <p><b>Status</b><br/> " . $view->result[$view->row_index]->field_field_prog_track_status[0]['rendered']['#markup']."</p>
-            <p><b>Status Note</b><br/> ".$view->result[$view->row_index]->field_field_prog_track_status_note[0]['rendered']['#markup']."</p><p>Please <a href='http://www2.epa.gov/lead/forms/contact-us' target='_blank'>contact the Lead Help Desk</a> if you have any questions about the status of your application.</p>
+            <p><span class='modal-label'>Application Information</span><br/>" . $view->result[$view->row_index]->field_field_prog_tracker_app[0]['rendered']['#markup']."</p>
+            <p><span class='modal-label'>Status</span><br/><strong> " . $view->result[$view->row_index]->field_field_prog_track_status[0]['rendered']['#markup']."</strong></p>
+            <p><span class='modal-label'>Status Note</span><br/> ".$view->result[$view->row_index]->field_field_prog_track_status_note[0]['rendered']['#markup']."</p><br/><p>Please contact the Lead Hotline at 1-800-424-LEAD [5323] if you have any questions about the status of your application.</p>
           </div>";
-    print "<br><a href='.' class='simple-dialog' id='details-link-" . $unique_id . "' rel='width:900;resizable:false;position:[center,center]' name='modal-page-details-prog-track-" . $unique_id . "' title='Status Note'>Details</a>";
+    print "<br/><a href='.' class='simple-dialog' id='details-link-" . $unique_id . "' rel='width:900;resizable:false;position:[center,center]' name='modal-page-details-prog-track-" . $unique_id . "' title='Status Note'>Details</a>";
 }
 ?>
