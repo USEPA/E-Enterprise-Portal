cf7ccab3fa92e84e4ba337a13d638d4f744af4b3
Jeff Soosiah <jsoosiah@gmail.com>
2015-09-02 16:15:17 -0400

    remove topics of interest

--- drupal/sites/all/modules/custom/features/first_time_user_profile_block/views-view--first-time-user-profile.tpl.php
+++ drupal/sites/all/modules/custom/features/first_time_user_profile_block/views-view--first-time-user-profile.tpl.php
@@ -2,52 +2,105 @@
 
 /**
  * @file
- * View: First Time User Profile
+ * Main view template.
  *
- * Required Drupal / configuration elements:
- * - People > Account Settings > First Time User (field_first_time_user)
- * - Views > First Time User Profile > Set field to User: First Time User (First Time User)
- * - Panels > Workbench Page > Edit > Add View:First Time User Profile to panel
+ * Variables available:
+ * - $classes_array: An array of classes determined in
+ *   template_preprocess_views_view(). Default classes are:
+ *     .view
+ *     .view-[css_name]
+ *     .view-id-[view_name]
+ *     .view-display-id-[display_name]
+ *     .view-dom-id-[dom_id]
+ * - $classes: A string version of $classes_array for use in the class attribute
+ * - $css_name: A css-safe version of the view name.
+ * - $css_class: The user-specified classes names, if any
+ * - $header: The view header
+ * - $footer: The view footer
+ * - $rows: The results of the view query, if any
+ * - $empty: The empty text to display if the view is empty
+ * - $pager: The pager next/prev links to display, if any
+ * - $exposed: Exposed widget form/info to display
+ * - $feed_icon: Feed icon to display, if any
+ * - $more: A link to view more, if any
  *
  * @ingroup views_templates
  */
 ?>
 
 <?php
+
 drupal_add_js(drupal_get_path('module', 'first_time_user_profile_block') . "/js/first_time_user_profile.js", "file");
 drupal_add_css(drupal_get_path('module', 'first_time_user_profile_block') . "/css/first_time_user_profile.css", "file");
+
 ?>
 <div id="first-time-user-profile">
-    <h2><span>Getting Started</span>What matters to you?</h2>
-    <p>We have many resources and will be adding more. To show you things that interest you, we
-have a few questions below. You can adjust these later in 'My account' too.</p>
-    <h3>Locations that interest you</h3>
-    <p id="location-description-intro">Pick a location to see environmental information for that area.   <span id="location-description-na">Until you choose a location, the default location will be Durham, <abbr title="North Carolina">NC</abbr>.</span></p>
-    <div id="zip_container">
-		<div id="location-description-user">
-			<span id="location-description-geo">Location</span>
-			<span id="nearest-location" aria-labelledby="location-description-geo">No location found</span>
-			<a href="#" id="change-location">Change location</a>
-		</div>
-    </div><!-- @end zip_container-->
-    <div id="location-add-new" style="display:none">
-        <span id="new-location">
-        	<label for="new-location-input">Enter city, state or ZIP code</label> <input id="new-location-input"/>
-            <button class="btn btn-primary btn-sm" id="add-location">Find</button>
-        </span>
-        <span style="display:none" id="choose-zip-holder">
-            <label for="choose-zip">Select your ZIP code</label><span id="choose-zip"></span>
-			<button class="btn btn-primary btn-sm" id="confirm-zip-select">Save</button>
-        </span>
-        <button class="btn btn-danger btn-sm" id="cancel-zip-select">Cancel</button>
-    </div><!-- @end location-add-new -->
-    <h3>Topics that matter</h3>
-    <p>Select a few high-level topics that interest you.</p>
-    <div id="high-level-interests">
-    <?php high_level_taxonomy_checkboxes(); ?>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="col-xs-12">
+                <h2>GETTING STARTED</h2>
+
+                <h1>What matters to you?</h1>
+
+                <h3>So many resources and we'll be adding more. To show you things that interest you, we
+                    have a few questions below. You can adjust these later in 'My account' too.</h3>
+            </div>
+            <div id="locations-holder" class="col-xs-12">
+
+
+                <h2>Locations that interest you</h2>
+
+                <div id="zip_container">
+                    <div class="col-xs-4">
+                        <div id="location-description-geo">
+                            We do not know your service provider's location. We have started your profile with a default of Durham, North Carolina. Feel free to change the zip code here.
+                        </div>
+                        <div id="location-description-user">
+                            <span id="locations-from-input-text">Your location from input</span> <br/> <a href="#" id="revert-to-geo-location">Revert to service provider's location </a>
+                        </div>
+                    </div>
+                    <div class="col-xs-4">
+                        <div id="nearest-location">No Location found</div>
+                    </div>
+                    <div class="col-xs-4">
+                        <a href="#" id="change-location">Change Location</a>
+                    </div>
+                    <!--                    <div class="col-xs-4">-->
+                    <!--                        <button id="add-zip-code" class="btn btn-success">+</button>-->
+                    <!--                    </div>-->
+                </div>
+                <div class="col-xs-6">
+
+                    <div style="display:none" id="new-location">
+                        <div class="col-xs-6"><label for="new-location-input">Enter City, State or ZIP code</label> <input
+                                id="new-location-input"/>
+                        </div>
+                        <div class="col-xs-6">
+                            <button class="btn btn-default btn-sm" id="add-location">Enter</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-6">
+                    <div style="display:none" id="choose-zip-holder">
+                        <div class="col-xs-6"> Select your ZIP code. <span id="choose-zip"></span>
+                        </div>
+                        <div class="col-xs-6">
+                            <button class="btn btn-default btn-sm" id="confirm-zip-select">Confirm</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12">
+                    <button style="display: none" class="btn btn-danger btn-sm pull-right" id="cancel-zip-select">Cancel</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12">
+            <div class="pull-right">
+                <button class="btn btn-md btn-primary" id="save-preferences">Save preferences</button>
+                <a href="#" id="skip-preferences">Skip this</a>
+            </div>
+        </div>
+
     </div>
-</div><!-- @end first-time-user-profile-->
-<div class="modal-footer">
-	<button  class="btn btn-md btn-primary" id="save-preferences">Save preferences</button>
-    <a href="#" id="skip-preferences">Skip this</a>
-</div><!-- @end modal-footer-->
+</div>
+
