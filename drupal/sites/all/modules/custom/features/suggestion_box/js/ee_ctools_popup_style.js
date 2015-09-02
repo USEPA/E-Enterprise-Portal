4e7a54f300c54271bf097c7c016f6e48c9cb6aef
Kate Walser <kate@cxinsights.com>
2015-09-02 13:26:05 -0400

    EE-560:  Modal X button * Adds ctools-modal-close class to button so event listener registers click to close modal

--- drupal/sites/all/modules/custom/features/suggestion_box/js/ee_ctools_popup_style.js
+++ drupal/sites/all/modules/custom/features/suggestion_box/js/ee_ctools_popup_style.js
@@ -4,7 +4,7 @@ Drupal.theme.prototype.ee_ctools_popup_theme = function () {
   html += '    <div class="ctools-modal-content">' // panels-modal-content
   html += '      <div class="modal-header ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix">';
   html += '        <span id="modal-title" class="modal-title ui-dialog-title"> </span>';
-  html += '          <button type="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only ui-dialog-titlebar-close" role="button" aria-disabled="false" title="close">';
+  html += '          <button type="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only ui-dialog-titlebar-close ctools-close-modal" role="button" aria-disabled="false" title="close">';
   html += '            <span class="ui-button-icon-primary ui-icon ui-icon-closethick"></span>';
   html += '            <span class="ui-button-text">close</span>';
   html += '      </div>';
