ec901c0780eb008044228cb691e94da1552751fa
Jeff Soosiah <jsoosiah@gmail.com>
2015-09-03 21:05:46 -0400

    first commit for gridstack updates

--- drupal/sites/all/themes/eenterprise/js/grid-launch.js
+++ drupal/sites/all/themes/eenterprise/js/grid-launch.js
@@ -1,14 +1,40 @@
 (function ($) {
-    $(document).ready(function(){
-        var options = {
-            static_grid: true,
-            vertical_margin: 10,
-            cell_height: 70,
-            handle: 'grid-stack-item'
-        };
+  $(document).ready(function(){
 
-        $('.grid-stack').gridstack(options);
+    var cellHeight = 70;
+    var verticalMargin = 10;
+
+    function recalculateWidgetHeights() {
+      $('.grid-stack-item-content').each(function(){
+        var gsHeight = Math.round((this.scrollHeight + verticalMargin * 2) / cellHeight);
+        console.log(gsHeight);
+        $(this).closest('.grid-stack-item').attr('data-gs-height', gsHeight);
+      });
+    }
+
+    var count = 0;
+
+    $(".grid-stack-item").each(function(){
+
+      var x = count % 2 * 6;
+      var y = Math.floor(count / 2) * 6;
+
+      $(this).attr({'data-gs-x': x, 'data-gs-y': y});
+      count++;
 
     });
-})(jQuery);
 
+    var options = {
+      static_grid: true,
+      vertical_margin: verticalMargin,
+      cell_height: cellHeight,
+      //handle: 'grid-stack-item'
+      //float:true
+    };
+
+    setTimeout(function(){
+      recalculateWidgetHeights();
+      $('.grid-stack').gridstack(options);
+    }, 5000);
+  });
+})(jQuery);
