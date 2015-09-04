b52ca68be819f5f703696168a2fc50ac36c5057c
Jeff Soosiah <jsoosiah@gmail.com>
2015-09-03 22:04:16 -0400

    gridstack updates

--- drupal/sites/all/themes/eenterprise/js/grid-launch.js
+++ drupal/sites/all/themes/eenterprise/js/grid-launch.js
@@ -4,37 +4,42 @@
     var cellHeight = 70;
     var verticalMargin = 10;
 
-    function recalculateWidgetHeights() {
-      $('.grid-stack-item-content').each(function(){
-        var gsHeight = Math.round((this.scrollHeight + verticalMargin * 2) / cellHeight);
-        console.log(gsHeight);
-        $(this).closest('.grid-stack-item').attr('data-gs-height', gsHeight);
+    function recalculateWidgetHeights(grid) {
+      $('.grid-stack-item').each(function(){
+        var contentHeight = $(this).find('.pane-title').outerHeight() + $(this).find('.pane-content').outerHeight() + verticalMargin * 2;
+        var gsHeight = Math.round((contentHeight + verticalMargin * 2) / cellHeight);
+        grid.resize(this, null, gsHeight);
       });
     }
 
+    // assign x and y values to widgets
+    // todo: load saved x and y values from user profile
     var count = 0;
-
     $(".grid-stack-item").each(function(){
-
       var x = count % 2 * 6;
       var y = Math.floor(count / 2) * 6;
-
       $(this).attr({'data-gs-x': x, 'data-gs-y': y});
       count++;
-
     });
 
     var options = {
       static_grid: true,
       vertical_margin: verticalMargin,
-      cell_height: cellHeight,
-      //handle: 'grid-stack-item'
-      //float:true
+      cell_height: cellHeight
     };
 
-    setTimeout(function(){
-      recalculateWidgetHeights();
-      $('.grid-stack').gridstack(options);
-    }, 5000);
+    $('.grid-stack').gridstack(options);
+    var grid = $('.grid-stack').data('gridstack');
+
+    // todo: find a way to recalculate widget heights less often
+    $(document).ajaxComplete(function(){
+      recalculateWidgetHeights(grid);
+    });
+
+    //$('.grid-stack').on('resizestop', function (e, items) {
+    //  console.log("?");
+      //recalculateWidgetHeights(grid);
+    //});
+    $( window ).resize( _.debounce( function(){ recalculateWidgetHeights(grid) }, 150 ) );
   });
 })(jQuery);
--- drupal/sites/all/themes/eenterprise/panels-pane.tpl.php
+++ drupal/sites/all/themes/eenterprise/panels-pane.tpl.php
@@ -20,7 +20,7 @@
 <?php if ($pane_prefix): ?>
   <?php print $pane_prefix; ?>
 <?php endif; ?>
-<div class="grid-stack-item" data-gs-width="6">
+<div class="grid-stack-item" data-gs-width="6" data-gs-height="6" data-gs-no-resize="true" data-gs-no-move="true" style="opacity:1.0">
     <div class="<?php print $classes; ?>" <?php print $id; ?> <?php print $attributes; ?>>
       <?php if ($admin_links): ?>
         <?php print $admin_links; ?>
