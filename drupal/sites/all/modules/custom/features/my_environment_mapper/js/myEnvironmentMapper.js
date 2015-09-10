45806b54408df1b6feaca4a7209cce5bc7374ff0
bgaines <Brett.Gaines@cgifederal.com>
2015-09-10 16:57:24 -0500

    Fix MyEnvironment Mapper bug for not updating when clicking update button

--- drupal/sites/all/modules/custom/features/my_environment_mapper/js/myEnvironmentMapper.js
+++ drupal/sites/all/modules/custom/features/my_environment_mapper/js/myEnvironmentMapper.js
@@ -11,7 +11,7 @@
         updateMyEnvMapperLoc(data);
       }
       previousZip = currentZip;
-    })
+    });
   });
 
   function updateMyEnvMapperLoc(data) {
@@ -19,17 +19,14 @@
       if (data.latitude > 0 && data.latitude < 90) {
         var zipCentLat = String(data.latitude);
         var zipCentLon = String(data.longitude);
-        //console.log("MyEnv map with lat/long: " + zipCentLat + " " + zipCentLon);
         setiFrameNewURL(zipCentLat, zipCentLon);
       }
     }
   }
 
   function setiFrameNewURL(zipCentLat, zipCentLon) {
-    var iFrameURL = "https://map11.epa.gov/myem/envmap/mainmap.html?pTheme=all&pLayers=afs,triair,triwater,rcra,tsca&ve=11," + zipCentLat + "," + zipCentLon;
-
+    var iFrameURL = "https://map11.epa.gov/myem/envmapEEP/mainmap.html?pTheme=all&pLayers=afs,triair,triwater,rcra,tsca&ve=11," + zipCentLat + "," + zipCentLon;
     $('#myEnviFrame').attr('src', iFrameURL);
-    //document.getElementById('myEnviFrame').src = iFrameURL;
   }
 
 })(jQuery);
