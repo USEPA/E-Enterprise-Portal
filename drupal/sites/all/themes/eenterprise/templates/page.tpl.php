341f014a0d441a67b95cc60a462c2cc5e071f326
ZTEGEGN <zerihun.tegegn@cgifederal.com>
2015-10-05 11:26:01 -0400

    SonarQube cleanup. remove tabs and add curly braces.

--- drupal/sites/all/themes/eenterprise/templates/page.tpl.php
+++ drupal/sites/all/themes/eenterprise/templates/page.tpl.php
@@ -5,12 +5,15 @@
  */
 ?>
 <?php
+define("HOME_PAGE","Home");
 if(drupal_is_front_page()) {
     global $user;
-    if($user->uid == 0)
-      drupal_goto("/eenterprise-new");
-    else
-      drupal_goto("/workbench");
+    if($user->uid == 0) {
+        drupal_goto("/eenterprise-new");
+    }
+    else {
+        drupal_goto("/workbench");
+    }
     exit;
 }
 if(arg(0) == 'workbench' && $user->uid == 0){
@@ -20,36 +23,36 @@ if(arg(0) == 'workbench' && $user->uid == 0){
 ?>
 <?php print render($page['alert']); ?>
 <header class="masthead clearfix" role="banner">
-	<?php
-		$hgroup_open = '';
-		$hgroup_close = '';
-	    if ($site_name || $site_slogan) {
-			$hgroup_open = '<hgroup class="site-name-and-slogan">';
-			$hgroup_close = '</hgroup>';
-		}
-	?>
-	<?php print $hgroup_open; ?>
-	<?php if ($site_name): ?>
-		<h1 class="site-name" id="site-name">
-			<?php $logoless = theme_get_setting('eenterprise_logoless'); ?>
-			<?php if ($logoless): ?>
-				<a class="logoless" href="<?php print $front_page; ?>" title="<?php print t('Home - '.$site_name); ?>" rel="home">
-				<span class="ee-header-line1">E-Enterprise <span class="logo-sublabel">for the </span></span><span class="ee-header-line2">Environment</span>
-			<?php elseif ($logo): ?>
-				<a href="<?php print $front_page; ?>" title="<?php print t('Home - '.$site_name); ?>" rel="home">
-				<img src="<?php print $logo; ?>" alt="<?php print t('Home - '.$site_name); ?>" class="site-logo" />
-			<?php else: ?>
-				<a href="<?php print $front_page; ?>" title="<?php print t('Home - '.$site_name); ?>" rel="home">
-				<img src="/sites/all/themes/eenterprise/images/placeholder-logo.png" alt="<?php print t('Home - '.$site_name); ?>" class="site-logo" />			
-			<?php endif; ?>
-			</a>
-		</h1>
-	<?php endif; ?>
-	<?php if ($site_slogan): ?>
-		<div class="site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
-	<?php endif; ?>
-	<?php print $hgroup_close; ?>
-	<?php print render($page['header']); ?>
+    <?php
+        $hgroup_open = '';
+        $hgroup_close = '';
+        if ($site_name || $site_slogan) {
+            $hgroup_open = '<hgroup class="site-name-and-slogan">';
+            $hgroup_close = '</hgroup>';
+        }
+    ?>
+    <?php print $hgroup_open; ?>
+    <?php if ($site_name): ?>
+        <h1 class="site-name" id="site-name">
+            <?php $logoless = theme_get_setting('eenterprise_logoless'); ?>
+            <?php if ($logoless): ?>
+                <a class="logoless" href="<?php print $front_page; ?>" title="<?php print t(HOME_PAGE.' - '.$site_name); ?>" rel="home">
+                <span class="ee-header-line1">E-Enterprise <span class="logo-sublabel">for the </span></span><span class="ee-header-line2">Environment</span>
+            <?php elseif ($logo): ?>
+                <a href="<?php print $front_page; ?>" title="<?php print t(HOME_PAGE.' - '.$site_name); ?>" rel="home">
+                <img src="<?php print $logo; ?>" alt="<?php print t(HOME_PAGE.' - '.$site_name); ?>" class="site-logo" />
+            <?php else: ?>
+                <a href="<?php print $front_page; ?>" title="<?php print t(HOME_PAGE.' - '.$site_name); ?>" rel="home">
+                <img src="/sites/all/themes/eenterprise/images/placeholder-logo.png" alt="<?php print t(HOME_PAGE.' - '.$site_name); ?>" class="site-logo" />
+            <?php endif; ?>
+            </a>
+        </h1>
+    <?php endif; ?>
+    <?php if ($site_slogan): ?>
+        <div class="site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
+    <?php endif; ?>
+    <?php print $hgroup_close; ?>
+    <?php print render($page['header']); ?>
 </header>
 <?php print render($page['navigation']); ?>
 <!-- @todo - Add content_language back in next line - section -->
@@ -57,8 +60,8 @@ if(arg(0) == 'workbench' && $user->uid == 0){
   <?php print render($page['preface']); ?>
   <?php print render($page['highlighted']); ?>
   <div id="content" class="main-column column clearfix" role="main">
-	<a id="main-content"></a>
-	<?php print render($page['help']); ?>
+    <a id="main-content"></a>
+    <?php print render($page['help']); ?>
     <?php print render($title_prefix); ?>
     <?php if ($title): ?>
     <?php
@@ -71,19 +74,19 @@ if(arg(0) == 'workbench' && $user->uid == 0){
       <h1 <?php print $title_attributes; ?>><?php print $title;?></h1>
       <!--googleoff: all-->
     <?php endif; ?>
-	<?php print render($title_suffix); ?>
-	<?php print render($tabs); ?>
-	<?php print $messages; ?>
-	<?php if ($action_links = render($action_links)): ?>
-		<ul class="action-links"><?php print render($action_links); ?></ul>
-	<?php endif; ?>
-	
+    <?php print render($title_suffix); ?>
+    <?php print render($tabs); ?>
+    <?php print $messages; ?>
+    <?php if ($action_links = render($action_links)): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+    <?php endif; ?>
+    
     <!--googleon: all-->
     <?php print render($page['content']); ?>
     <!--googleoff: all-->
     <?php print $feed_icons; ?>
 
-	</div>
+    </div>
     
     <?php
       // Render the sidebars to see if there's anything in them.
