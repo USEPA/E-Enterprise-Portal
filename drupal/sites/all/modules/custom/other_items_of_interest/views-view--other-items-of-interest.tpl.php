31a58df32a39677cf47a69bea633a9271176cf24
bmatkin <benjamin.matkin@cgifederal.com>
2015-11-19 07:54:02 -0600

    EE-434 reordering js

--- drupal/sites/all/modules/custom/other_items_of_interest/views-view--other-items-of-interest.tpl.php
+++ drupal/sites/all/modules/custom/other_items_of_interest/views-view--other-items-of-interest.tpl.php
@@ -1,8 +1,8 @@
 <?php
     $module_name = "other_items_of_interest";
-    drupal_add_js(drupal_get_path('module', $module_name) . "/js/other_items_of_interest.js", "file");
     drupal_add_js(drupal_get_path('module', $module_name) . "/DataTables/js/jquery.dataTables.min.js", "file");
     drupal_add_css(drupal_get_path('module', $module_name) . "/DataTables/css/jquery.dataTables.min.css", "file");
+    drupal_add_js(drupal_get_path('module', $module_name) . "/js/other_items_of_interest.js", "file");
 
 
     $temp_content_data = array(
@@ -25,8 +25,9 @@
         )
     );
 
-    if (count($temp_content_data) > 0) {
-        generateAreasOfInterestTable($temp_content_data);
+    $state_resources = loadStateResources();
+    if (count($state_resources) > 0) {
+        generateAreasOfInterestTable($state_resources);
     }
     else {
         echo "Currently there are no available Other Areas of Interest";
