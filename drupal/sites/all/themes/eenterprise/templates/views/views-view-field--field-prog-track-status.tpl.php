bfe41901453815ba2d3f74fbcd2b53a347be8203
ZTEGEGN <zerihun.tegegn@cgifederal.com>
2015-11-20 14:28:31 -0500

    Remove details from Item column in progress tracker, EE-725

--- drupal/sites/all/themes/eenterprise/templates/views/views-view-field--field-prog-track-status.tpl.php
+++ drupal/sites/all/themes/eenterprise/templates/views/views-view-field--field-prog-track-status.tpl.php
@@ -6,8 +6,10 @@ print "<div id='modal-page-details-prog-track-" . $unique_id . "' class='modal-c
 
 if(!empty($view->result[$view->row_index]->field_field_prog_track_status_note))
     print "<p><span class='modal-label'>Status Note</span><br/> ".$view->result[$view->row_index]->field_field_prog_track_status_note[0]['rendered']['#markup']."</p>";
-
-print "<br/><p>Please contact the Lead Hotline at 1-800-424-LEAD [5323] if you have any questions about the status of your application.</p>";
+if($view->result[$view->row_index]->field_field_prog_track_domain[0]['rendered']['#markup'] == 'Lead')
+    print "<br/><p>Please contact the Lead Hotline at 1-800-424-LEAD [5323] if you have any questions about the status of your application.</p>";
+else if ($view->result[$view->row_index]->field_field_prog_track_domain[0]['rendered']['#markup'] == 'CEDRI')
+    print "<p><span class='modal-label'>Other Information</span><br/> ".$view->result[$view->row_index]->field_field_prog_track_part_code[0]['rendered']['#markup']." - ".$view->result[$view->row_index]->field_field_prog_track_part_name[0]['rendered']['#markup']."<br/> ".$view->result[$view->row_index]->field_field_prog_track_sub_part_code[0]['rendered']['#markup']." - ".$view->result[$view->row_index]->field_field_prog_track_sub_part_name[0]['rendered']['#markup']."</p>";
 print "</div>";
 $title_value = $view->result[$view->row_index]->field_field_prog_tracker_app[0]['rendered']['#markup'];
 print "<br/><a href='.' class='simple-dialog' id='details-link-" . $unique_id . "' rel='width:900;resizable:false;position:[center,center]' name='modal-page-details-prog-track-" . $unique_id . "' title='Details for ".$title_value."'>".$output."</a>";
