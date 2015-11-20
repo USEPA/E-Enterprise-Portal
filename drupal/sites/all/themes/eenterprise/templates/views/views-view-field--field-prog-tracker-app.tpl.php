6d1a34886c2af189193908722d72bc8211373e4d
ZTEGEGN <zerihun.tegegn@cgifederal.com>
2015-11-20 10:31:05 -0500

    Remove details from Item column in progress tracker, EE-725

--- drupal/sites/all/themes/eenterprise/templates/views/views-view-field--field-prog-tracker-app.tpl.php
+++ drupal/sites/all/themes/eenterprise/templates/views/views-view-field--field-prog-tracker-app.tpl.php
@@ -27,24 +27,4 @@ if(!empty($view->result[$view->row_index]->field_field_prog_track_facility_name)
     print "<br><span class='item-subscript-text'>".$view->result[$view->row_index]->field_field_prog_track_facility_name[0]['rendered']['#markup']."</span>";
 }
 
-if(!empty($view->result[$view->row_index]->field_field_prog_track_facility_name)) {
-    $unique_id = $view->query->pager->current_page.'-'.$view->row_index;
-    print "<div id='modal-page-details-prog-track-" . $unique_id . "' class='modal-content-in-page'>
-            <p><span class='modal-label'>Reporting Facility</span><br/> ".$view->result[$view->row_index]->field_field_prog_track_facility_name[0]['rendered']['#markup']." (".$view->result[$view->row_index]->field_field_prog_track_facility_reg_id[0]['rendered']['#markup'].")</p>
-            <p><span class='modal-label'>Report will be published on</span><br/><strong> " . $view->result[$view->row_index]->field_field_prog_track_updated[0]['rendered']['#markup']."</strong></p>
-            <p><span class='modal-label'>Other Information</span><br/> ".$view->result[$view->row_index]->field_field_prog_track_part_code[0]['rendered']['#markup']." - ".$view->result[$view->row_index]->field_field_prog_track_part_name[0]['rendered']['#markup']."<br/>".$view->result[$view->row_index]->field_field_prog_track_sub_part_code[0]['rendered']['#markup']." - ".$view->result[$view->row_index]->field_field_prog_track_sub_part_name[0]['rendered']['#markup']."</div>";
-
-    $title_value = $view->result[$view->row_index]->field_field_prog_track_facility_name[0]['rendered']['#markup'];
-    print "<br><a href='.' class='simple-dialog' id='details-link-" . $unique_id . "' rel='width:900;resizable:false;position:[center,center]' name='modal-page-details-prog-track-" . $unique_id . "' title='Details for ".$title_value."'>Details</a>";
-}
-else if(!empty($view->result[$view->row_index]->field_field_prog_track_status_note)) {
-    $unique_id = $view->query->pager->current_page.'-'.$view->row_index;
-    print "<div id='modal-page-details-prog-track-" . $unique_id . "' class='modal-content-in-page'>
-            <p><span class='modal-label'>Application Information</span><br/>" . $view->result[$view->row_index]->field_field_prog_tracker_app[0]['rendered']['#markup']."</p>
-            <p><span class='modal-label'>Status</span><br/><strong> " . $view->result[$view->row_index]->field_field_prog_track_status[0]['rendered']['#markup']."</strong></p>
-            <p><span class='modal-label'>Status Note</span><br/> ".$view->result[$view->row_index]->field_field_prog_track_status_note[0]['rendered']['#markup']."</p><br/><p>Please contact the Lead Hotline at 1-800-424-LEAD [5323] if you have any questions about the status of your application.</p>
-          </div>";
-    $title_value = $view->result[$view->row_index]->field_field_prog_tracker_app[0]['rendered']['#markup'];
-    print "<br/><a href='.' class='simple-dialog' id='details-link-" . $unique_id . "' rel='width:900;resizable:false;position:[center,center]' name='modal-page-details-prog-track-" . $unique_id . "' title='Details for ".$title_value."'>Details</a>";
-}
 ?>
