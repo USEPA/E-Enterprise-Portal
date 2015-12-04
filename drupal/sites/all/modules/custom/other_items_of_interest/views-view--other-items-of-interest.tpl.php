db74698fcbbc7759d0c67853a67d40ae23ceca85
bmatkin <benjamin.matkin@cgifederal.com>
2015-12-04 15:15:32 -0600

    EE-434 adding feature other items of interest

--- drupal/sites/all/modules/custom/other_items_of_interest/views-view--other-items-of-interest.tpl.php
+++ drupal/sites/all/modules/custom/other_items_of_interest/views-view--other-items-of-interest.tpl.php
@@ -1,50 +1,40 @@
 <?php
-    $module_name = "other_items_of_interest";
-    
-    drupal_add_css("sites/all/libraries/jqueryui/themes/base/jquery.ui.tabs.css", "file");
-		drupal_add_js("sites/all/libraries/jqueryui/ui/jquery.ui.tabs.js", "file");
-    
-    drupal_add_js(drupal_get_path('module', $module_name) . "/DataTables/js/jquery.dataTables.min.js", "file");
-    drupal_add_css(drupal_get_path('module', $module_name) . "/DataTables/css/jquery.dataTables.min.css", "file");
-    drupal_add_js(drupal_get_path('module', $module_name) . "/js/other_items_of_interest.js", "file");
-    drupal_add_css(drupal_get_path('module', $module_name) . "/css/other_items_of_interest.css", "file");
+$module_name = "other_items_of_interest";
 
+drupal_add_css("sites/all/libraries/jqueryui/themes/base/jquery.ui.tabs.css", "file");
+drupal_add_js("sites/all/libraries/jqueryui/ui/jquery.ui.tabs.js", "file");
+
+drupal_add_js(drupal_get_path('module', $module_name) . "/DataTables/js/jquery.dataTables.min.js", "file");
+drupal_add_css(drupal_get_path('module', $module_name) . "/DataTables/css/jquery.dataTables.min.css", "file");
+drupal_add_js(drupal_get_path('module', $module_name) . "/js/other_items_of_interest.js", "file");
+drupal_add_css(drupal_get_path('module', $module_name) . "/css/other_items_of_interest.css", "file");
 
 
 ?>
-<<<<<<< HEAD
-<button id="restrict-to-current-button" type="button"></button>
-    <button id="restrict-to-states-button" type="button" class="inactive">Favorite Locations</button>
-        <button id="all-states-button" type="button" class="inactive">All Locations</button>
-=======
 
 <div id="other-areas-tabs">
-    <div class="action-links"><a class="favorites-ignore first" href="/new-state-resource">Add state resource</a> <a href="/state-resource-editor" class="favorites-ignore last">Edit</a></div>
+    <?php
+    // Load the currently logged in user.
+    global $user;
 
->>>>>>> 1a98fde7870ab6f0756a364369b6d5717339b16c
-<?php
-// Load the currently logged in user.
-global $user;
+    // Check if the user has the 'editor' role.
+    if (in_array('state_admin', $user->roles)) {
+        ?>
 
-// Check if the user has the 'editor' role.
-if (in_array('state_admin', $user->roles)) {
+        <div class="action-links"><a class="favorites-ignore first" href="/new-state-resource">Add state resource</a> <a href="/state-resource-editor" class="favorites-ignore last">Edit</a></div>
+    <?php
+    }
     ?>
 
-  <ul>
-    <li id="restrict-to-current-button"><a class="favorites-ignore" href="#current-state-resources"></a></li>
-    <li id="restrict-to-states-button"><a class="favorites-ignore" href="#favorite-state-resources">My locations</a></li>
-    <li id="all-states-button"><a class="favorites-ignore" href="#all-state-resources">All</a></li>
-  </ul>
 
-<?php
-}
-?>
-<div id="current-state-resources"></div>
-<div id="favorite-state-resources"></div>
-<div id="all-state-resources"></div>
-<<<<<<< HEAD
-=======
+        <ul>
+            <li id="restrict-to-current-button"><a class="favorites-ignore" href="#current-state-resources"></a></li>
+            <li id="restrict-to-states-button"><a class="favorites-ignore" href="#favorite-state-resources">My locations</a></li>
+            <li id="all-states-button"><a class="favorites-ignore" href="#all-state-resources">All</a></li>
+        </ul>
+
+    <div id="current-state-resources"></div>
+    <div id="favorite-state-resources"></div>
+    <div id="all-state-resources"></div>
 </div>
->>>>>>> 1a98fde7870ab6f0756a364369b6d5717339b16c
-
 
