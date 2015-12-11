16f1f8f787358479f78d40ab13d89daf11e21134
bmatkin <benjamin.matkin@cgifederal.com>
2015-12-11 00:06:35 -0600

    EE-294 remove bug

--- drupal/sites/all/themes/eenterprise/panels-pane.tpl.php
+++ drupal/sites/all/themes/eenterprise/panels-pane.tpl.php
@@ -33,7 +33,6 @@ else {
 
 <?php if ($pane_prefix): ?>
   <?php print $pane_prefix; ?>;
-    $user->name == 'guest-user'
 <?php endif; ?>
 <div class="grid-stack-item"  data-gs-height="30" style="opacity:1.0">
     <div class="<?php print $classes;?>" id="<?php echo "gridstack-pane-" . $pane->subtype; ?>" <?php print $attributes; ?>>
