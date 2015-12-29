50f95a80b62a6c85aee15f8a658cba29984eec00
bmatkin <benjamin.matkin@cgifederal.com>
2015-12-29 09:36:45 -0600

    EE-788 fixed the guest users location error

--- drupal/sites/all/modules/custom/other_items_of_interest/views-view--other-items-of-interest.tpl.php
+++ drupal/sites/all/modules/custom/other_items_of_interest/views-view--other-items-of-interest.tpl.php
@@ -16,7 +16,6 @@ drupal_add_css(drupal_get_path('module', $module_name) . "/css/other_items_of_in
     <?php
     // Load the currently logged in user.
     global $user;
-
     // Check if the user has the 'editor' role.
     if (in_array('state_admin', $user->roles)) {
         ?>
@@ -28,14 +27,27 @@ drupal_add_css(drupal_get_path('module', $module_name) . "/css/other_items_of_in
 
     <ul>
         <li id="restrict-to-current-button"><a class="favorites-ignore" href="#current-state-resources"></a></li>
-        <li id="restrict-to-states-button"><a class="favorites-ignore" href="#favorite-state-resources">My locations</a></li>
+        <?php
+        if ($user->name != 'guest-user') {
+            ?>
+            <li id="restrict-to-states-button"><a class="favorites-ignore" href="#favorite-state-resources">My
+                    locations</a></li>
+        <?php
+        }
+        ?>
         <li id="all-states-button"><a class="favorites-ignore" href="#all-state-resources">All</a></li>
         <li id="epa-button"><a class="favorites-ignore" href="#epa-resources">EPA</a></li>
 
     </ul>
 
     <div id="current-state-resources"></div>
-    <div id="favorite-state-resources"></div>
+    <?php
+    if ($user->name != 'guest-user') {
+        ?>
+        <div id="favorite-state-resources"></div>
+    <?php
+    }
+    ?>
     <div id="all-state-resources"></div>
     <div id="epa-resources"></div>
 </div>
