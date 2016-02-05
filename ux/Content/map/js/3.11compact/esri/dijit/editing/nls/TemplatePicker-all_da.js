74e33bcb7a0c6f1a8825549654d83a638f4cb961
ZTEGEGN <zerihun.tegegn@cgifederal.com>
2016-02-04 19:17:34 -0500

    EE-840 automated deployment for cloud.gov

---
+++ ux/Content/map/js/3.11compact/esri/dijit/editing/nls/TemplatePicker-all_da.js
@@ -0,0 +1,4 @@
+// All material copyright ESRI, All Rights Reserved, unless otherwise specified.
+// See http://js.arcgis.com/3.11/esri/copyright.txt for details.
+//>>built
+define("esri/dijit/editing/nls/TemplatePicker-all_da",{"dijit/nls/loading":{_localized:{},loadingState:"Indl\u00e6ser...",errorState:"Der er opst\u00e5et en fejl"}});
---
+++ ux/ux-patterns/widgets/tables.php
@@ -0,0 +1,1 @@
+<h3>Tables (Search Results, Data Summaries)</h3>
