ffe2e220dfaf28e274e6db49ce8c437ce4878d64
bmatkin <benjamin.matkin@cgifederal.com>
2016-05-06 09:23:49 -0500

    Updating local files

--- drupal/sites/all/modules/custom/cdx_facility_management/views-view--cdx-facility-management--block.tpl.php
+++ drupal/sites/all/modules/custom/cdx_facility_management/views-view--cdx-facility-management--block.tpl.php
@@ -1,27 +1,11 @@
 <?php
-drupal_add_js(drupal_get_path('module', 'cdx_facility_management') . "/js/custom_js.js", "file");
-drupal_add_js(drupal_get_path('module', 'cdx_facility_management') . "/js/cdx_facility_management.js", "file");
-?>
 
-<?php
-/**
- * @file
- * View: First Time User Profile
- *
- * Required Drupal / configuration elements:
- * - People > Account Settings > First Time User (field_first_time_user)
- * - Views > First Time User Profile > Set field to User: First Time User (First Time User)
- * - Panels > Workbench Page > Edit > Add View:First Time User Profile to panel
- *
- * @ingroup views_templates
- */
-drupal_add_css('https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css', 'external');
+//drupal_add_css('https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css', 'external');
 //drupal_add_js(drupal_get_path('module', 'cdx_facility_management') . "/custom_js.js", "file");
 drupal_add_css(drupal_get_path('module', 'cdx_facility_management') . "/css/cdx_facility_management.css", "file");
-
 drupal_add_library('system', 'ui.accordion');
-
 $cdx_facility_source = variable_get('cdx_facility_resource_url');
+
 ?>
 <!-- Site must incorporate these files to use FacilityWidget -->
 <!--<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>-->
@@ -34,8 +18,8 @@ $cdx_facility_source = variable_get('cdx_facility_resource_url');
 <!--<link rel="stylesheet" href="../libs/jquery-ui-1.11.4.custom/jquery-ui.min.css" />-->
 <?php echo '<link rel="stylesheet"
       href="' . str_replace('/FrsPhase2', '', $cdx_facility_source) .  '/ContentFramework/v3/js/third-party/fancybox-v2/jquery.fancybox.css"/>';?>
-<?php echo '<link rel="stylesheet"
-      href="' . str_replace('/FrsPhase2', '', $cdx_facility_source) .  '/ContentFramework/v3/css/font-awesome-4.0.3/css/font-awesome.min.css"/>';?>
+<?php //echo '<link rel="stylesheet"
+//      href="' . str_replace('/FrsPhase2', '', $cdx_facility_source) .  '/ContentFramework/v3/css/font-awesome-4.0.3/css/font-awesome.min.css"/>';?>
 
 <!--<link rel="stylesheet" href="https://dev.epacdx.net/ContentFramework/v3/css/s.css"></link>	-->
 <?php echo '<script type="text/javascript" src="' . str_replace('/FrsPhase2', '', $cdx_facility_source) .  '/ContentFramework/v3/js/third-party/jquery.blockUI.1.7.js"></script>';?>
@@ -43,9 +27,17 @@ $cdx_facility_source = variable_get('cdx_facility_resource_url');
 
 
 <!-- Below is mapping stuff -->
-<link rel="stylesheet" href="//js.arcgis.com/3.13/dijit/themes/claro/claro.css"/>
-<link rel="stylesheet" href="//js.arcgis.com/3.13/esri/css/esri.css"/>
-<script src="//js.arcgis.com/3.13/"></script>
+<?php
+dpm($module_path);
+drupal_add_css($module_path . "/css/claro.css", "file");
+drupal_add_css($module_path . "/css/esri.css", "file");
+drupal_add_js($module_path . "/js/arcgis_3_13_init.js", "file");
+
+?>
+<!--<link rel="stylesheet" href="/css/claro.css"/>-->
+<!--<link rel="stylesheet" href="/css/esri.css"/>-->
+<script src="/js/arcgis_3_13_init.js"></script>
+
 
 <?php echo '<link rel="stylesheet" href="' . $cdx_facility_source .  '/ContentFramework/FRS%20Widget/FacilityManagementStyles.css">';?>
 
@@ -55,6 +47,13 @@ $cdx_facility_source = variable_get('cdx_facility_resource_url');
 
 <?php echo '<script src="' . $cdx_facility_source .  '/ContentFramework/FRS%20Widget/FacilityManagementTemplates.min.js"></script>';?>
 <?php echo '<script src="' . str_replace('/FrsPhase2', '', $cdx_facility_source) .  '/ContentFramework/FRS%20Widget/src/FacilityManagementWidget.js"></script>';?>
+
+
+<?php
+drupal_add_js(drupal_get_path('module', 'cdx_facility_management') . "/js/custom_js.js", "file");
+drupal_add_js(drupal_get_path('module', 'cdx_facility_management') . "/js/cdx_facility_management.js", "file");
+?>
+
 
 <div id="cdx-logged-in-options">
 <div id="facility-widget" style="max-width:1363px;"></div>
