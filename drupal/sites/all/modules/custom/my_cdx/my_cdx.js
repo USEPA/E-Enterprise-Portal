9955323093c3d1179d718f56b8dd3d3aace02057
Christian Manalansan <christian.manalansan@cgifederal.com>
2016-07-21 10:26:57 -0400

    EE-1415 Adjust styling to match wireframe

--- drupal/sites/all/modules/custom/my_cdx/my_cdx.js
+++ drupal/sites/all/modules/custom/my_cdx/my_cdx.js
@@ -1,18 +1,4 @@
 (function ($) {
-  $.fn.dataTableExt.oApi.fnPagingInfo = function (oSettings) {
-    return {
-      "iStart": oSettings._iDisplayStart,
-      "iEnd": oSettings.fnDisplayEnd(),
-      "iLength": oSettings._iDisplayLength,
-      "iTotal": oSettings.fnRecordsTotal(),
-      "iFilteredTotal": oSettings.fnRecordsDisplay(),
-      "iPage": oSettings._iDisplayLength === -1 ?
-        0 : Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength),
-      "iTotalPages": oSettings._iDisplayLength === -1 ?
-        0 : Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength) - 1
-    };
-  };
-
   $(document).ready(function() {
     $('#my-cdx').find('table').DataTable({
       "ajax": Drupal.settings.basePath + 'my-cdx/json',
