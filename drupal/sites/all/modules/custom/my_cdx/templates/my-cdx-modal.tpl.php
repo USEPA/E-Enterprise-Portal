1fff46d359650322963d69828787bf05429b8542
Christian Manalansan <christian.manalansan@cgifederal.com>
2016-07-26 22:00:40 -0400

    EE-1416 Refactor $modal_html to come from a .tpl.php file

---
+++ drupal/sites/all/modules/custom/my_cdx/templates/my-cdx-modal.tpl.php
@@ -0,0 +1,14 @@
+<div class="my-cdx-modal">
+  <div>Organization Name</div>
+  <div class="organization-name"></div>
+  <select class="organization-select"></select>
+  <div>Program Client ID</div>
+  <div class="program-client-name"></div>
+  <select class="program-client-select"></select>
+  <div>Program</div>
+  <div class="program-acronym"></div>
+  <div class="operations">
+    <a href="#" class="proceed">Proceed</a>
+    <a href="#" class="cancel">Cancel</a>
+  </div>
+</div>
