316a86511cb0101eeae520a4b4e62631133efb46
bmatkin <benjamin.matkin@cgifederal.com>
2016-08-11 11:53:27 -0500

    EE-1102: Remove deprecated Menu Settings Remove CDX Drupal View. Instead use custom module block. Cleaned up CDX Facility module removing deprecated functions and the locallly stored wsdls

--- drupal/sites/all/modules/custom/cdx_facility_management/js/src/cdx_facility_management.js
+++ drupal/sites/all/modules/custom/cdx_facility_management/js/src/cdx_facility_management.js
@@ -4,9 +4,10 @@
     var widget_updating = false;
     var must_update_widget = false;
     var user_role_waiting;
+    var renew_token = Drupal.settings.renew_token_from_bridge;
 
     // Ajax request for the CDX token to authenticate Facility Widget
-    var create_token_ajax = $.ajax({
+    var renew_token_ajax = $.ajax({
         url: Drupal.settings.basePath + 'return_cdx_facility_management_token',
         type: 'JSON'
     });
@@ -18,7 +19,6 @@
         var program_single = $('#fmw-program-single');
         var type_filter_select_holder = $('#fmw-type-select-holder');
         var management_button = $('#launch-facility-management');
-
         var first_name = "";
         var count = 0;
         program_filter_select.html('<option value="">Select&hellip;</option>');
@@ -91,106 +91,105 @@
 
 
     function generateUserData() {
-        $.ajax({
-            url: Drupal.settings.basePath + 'retrieve_cdx_user_data',
-            success: function (json) {
-                var parsed_json = $.parseJSON(json);
-                if (!parsed_json.error) {
-                    var org_to_roles = parsed_json.organizations_to_roles;
-                    if (org_to_roles.length == 0) {
-                        // User does not manage any facilities
-                        var zero_facilities_msg = "<p>You do not have any facilities to manage in CDX through this widget at this time.</p>";
-                        $('#fmw-organization-select-holder').html(zero_facilities_msg);
+        var user_data = Drupal.settings.cdx_facility_widget_settings.user_data;
+        if (user_data.length === 0) {
+            console.log("CDX Error");
+            $('#fmw-organization-select-holder').html("Unable to receive user data.");
+            return;
+        }
+        if (user_data.error) {
+            console.log("CDX Error", user_data);
+            $('#fmw-organization-select-holder').html("Unable to receive user data.");
+            return;
+        }
+
+        var org_to_roles = user_data.organizations_to_roles;
+        if (org_to_roles.length === 0) {
+            // User does not manage any facilities
+            var zero_facilities_msg = "<p>You do not have any facilities to manage in CDX through this widget at this time.</p>";
+            $('#fmw-organization-select-holder').html(zero_facilities_msg);
+            return;
+        }
+
+        //initialize filters object
+        var org_filter_select = $('#fmw-organization-select');
+        var org_single = $('#fmw-organization-single');
+        var management_button = $('#launch-facility-management');
+
+        var type_filter_select_holder = $('#fmw-type-select-holder');
+
+        var program_filter_select_holder = $('#fmw-program-select-holder');
+        var token_data = Drupal.settings.cdx_facility_widget_settings.token_data;
+        if (token_data === "") {
+            token_data = {expired: true};
+        }
+
+        if (token_data.expired) {
+            userMustLogin();
+            return;
+        }
+
+        var token = token_data.token;
+        var naas_ip = token_data.server_ip;
+        var resource_url = Drupal.settings.cdx_facility_widget_settings.resource_url;
+        var time_logged_in = token_data.user_login_time;
+        var time_threshold = token_data.user_session_logout;
+        // First connect to widget initially to start CDX session;
+        var temp = 0;
+        $.each(org_to_roles, function (org_id, role_object) {
+            if (role_object.roles) {
+                $.each(role_object.roles, function (dataflow, role_array) {
+                    if (temp == 0) {
+                        var initial_user_role_id = role_array[0].userRoleId;
+                        updateWidget(initial_user_role_id, token, naas_ip, resource_url, time_logged_in, time_threshold, 0);
+                        temp = 1;
                     }
-                    else {
-
-                        //initialize filters object
-                        var org_filter_select = $('#fmw-organization-select');
-                        var org_single = $('#fmw-organization-single');
-                        var management_button = $('#launch-facility-management');
-
-                        var type_filter_select_holder = $('#fmw-type-select-holder');
-
-                        var program_filter_select_holder = $('#fmw-program-select-holder');
-                        // Wait for new token to finish
-                        create_token_ajax.done(function (token_data) {
-                            if (token_data == "") {
-                                token_data = {expired: true};
-                            }
-                            if (!token_data.expired) {
-                                var token = token_data.token;
-                                var naas_ip = token_data.server_ip;
-                                var resource_url = token_data.url;
-                                var time_logged_in = token_data.user_login_time;
-                                var time_threshold = token_data.user_session_logout;
-                                // First connect to widget initially to start CDX session;
-                                var temp = 0;
-                                $.each(org_to_roles, function (org_id, role_object) {
-                                    if (role_object.roles) {
-                                        $.each(role_object.roles, function (dataflow, role_array) {
-                                            if (temp == 0) {
-                                                var initial_user_role_id = role_array[0].userRoleId;
-                                                updateWidget(initial_user_role_id, token, naas_ip, resource_url, time_logged_in, time_threshold, 0);
-                                                temp = 1;
-                                            }
-                                        });
-                                    }
-                                });
-
-                                org_filter_select.append('<option value="">Select an Organization</option>');
-                                var count = 0;
-                                var first_org_id = "";
-                                var first_org_name = "";
-                                $.each(org_to_roles, function (org_id, org_obj) {
-                                    if (org_obj.roles) {
-                                        org_filter_select.append('<option value="' + org_id + '" >' + org_obj.name + '</option>');
-                                        count = count + 1;
-                                        first_org_id = org_id; // unused if count > 1
-                                        first_org_name = org_obj.name;
-                                    }
-                                });
-
-                                if (count == 1) {
-                                    org_filter_select.val(first_org_id).hide();
-                                    org_single.html(first_org_name).show();
-                                    createProgramSelect(org_to_roles[first_org_id].roles);
-                                }
-                                else {
-                                    org_filter_select.show();
-                                }
-
-
-                                org_filter_select.change(function () {
-                                    var selected_org = $(this).val();
-                                    program_filter_select_holder.hide();
-                                    type_filter_select_holder.hide();
-                                    management_button.hide();
-                                    if (selected_org != '') {
-                                        createProgramSelect(org_to_roles[selected_org].roles);
-                                    }
-                                });
-
-                                management_button.click(function () {
-                                    var user_role_id = $('#fmw-type-select').val();
-                                    updateWidget(user_role_id, token, naas_ip, resource_url, time_logged_in, time_threshold, 0);
-                                    if ($('#facility-widget').length > 0) {
-                                        cdx_facility_management_block.dialog('open');
-                                        $('.ui-dialog').focus();
-                                    }
-                                });
-
-                            }
-                            else {
-                                userMustLogin();
-                            }
-                        });
-                    }
-                } else {
-                    console.log("CDX Error", parsed_json);
-                    $('#fmw-organization-select-holder').html("Unable to receive user data.");
-                }
+                });
             }
         });
+
+        org_filter_select.append('<option value="">Select an Organization</option>');
+        var count = 0;
+        var first_org_id = "";
+        var first_org_name = "";
+        $.each(org_to_roles, function (org_id, org_obj) {
+            if (org_obj.roles) {
+                org_filter_select.append('<option value="' + org_id + '" >' + org_obj.name + '</option>');
+                count = count + 1;
+                first_org_id = org_id; // unused if count > 1
+                first_org_name = org_obj.name;
+            }
+        });
+
+        if (count == 1) {
+            org_filter_select.val(first_org_id).hide();
+            org_single.html(first_org_name).show();
+            createProgramSelect(org_to_roles[first_org_id].roles);
+        }
+        else {
+            org_filter_select.show();
+        }
+
+
+        org_filter_select.change(function () {
+            var selected_org = $(this).val();
+            program_filter_select_holder.hide();
+            type_filter_select_holder.hide();
+            management_button.hide();
+            if (selected_org != '') {
+                createProgramSelect(org_to_roles[selected_org].roles);
+            }
+        });
+
+        management_button.click(function () {
+            var user_role_id = $('#fmw-type-select').val();
+            updateWidget(user_role_id, token, naas_ip, resource_url, time_logged_in, time_threshold, 0);
+            if ($('#facility-widget').length > 0) {
+                cdx_facility_management_block.dialog('open');
+                $('.ui-dialog').focus();
+            }
+        });
+
     }
 
 
@@ -231,24 +230,30 @@
             NASSToken: naas_token,
             NAASip: naas_ip,
             onInvalidSession: function () {
-                create_token_ajax.done(function (new_token_return) {
-                    if (new_token_return.expired) {
-                        userMustLogin();
-                    }
-                    else {
-                        var new_naas_token = new_token_return.token;
-                        if (new_naas_token === '') {
-                            unableToConnectWidget("CDX Facility- Blank Token");
-                        }
-                        else if (number_attempts > 2) {
-                            unableToConnectWidget("CDX Facility- Max attempts.");
+                // If not using token from bridge, we can create a new token and Facility widget
+                if (!Drupal.settings.cdx_facility_widget_settings.token_from_bridge) {
+                    renew_token_ajax.done(function (renewed_token_return) {
+                        if (renewed_token_return.expired) {
+                            userMustLogin();
                         }
                         else {
-                            widget_updating = false;
-                            updateWidget(user_role_id, new_naas_token, naas_ip, resource_url, time_logged_in, time_threshold, number_attempts + 1);
+                            var new_naas_token = renewed_token_return.token;
+                            if (new_naas_token === '') {
+                                unableToConnectWidget("CDX Facility- Blank Token");
+                            }
+                            else if (number_attempts > 2) {
+                                unableToConnectWidget("CDX Facility- Max attempts.");
+                            }
+                            else {
+                                widget_updating = false;
+                                updateWidget(user_role_id, new_naas_token, naas_ip, resource_url, time_logged_in, time_threshold, number_attempts + 1);
+                            }
                         }
-                    }
-                });
+                    });
+                } else {
+                    // The initial token from the bridge has expired, use must login.
+                    userMustLogin();
+                }
             },
             onServiceCall: function () {
                 cdx_facility_management_block.dialog("option", "position", {
