630b22567e0c472d87e8cea04cdc19b6053a8cb6
smolinsk <samuel.molinski@cgifederal.com>
2016-09-26 13:07:46 -0500

    EE-1504: more layout things for treatment section

--- drupal/sites/all/modules/custom/be_well_informed/js/src/be_well_informed.js
+++ drupal/sites/all/modules/custom/be_well_informed/js/src/be_well_informed.js
@@ -66,6 +66,131 @@ function resetBWIForm() {
   sampleData =  function(sample) {
     if(!sample) {
       sample = {"CityName":"Anonymous","RoutineContaminants":{"As":{"Symbol":"As","Name":"Arsenic","Value":".048","Unit":"mg/L"},"Cl":{"Symbol":"Cl","Name":"Chloride","Value":"5.2","Unit":"mg/L"},"Cu":{"Symbol":"Cu","Name":"Copper","Value":".104","Unit":"mg/L"},"CuSt":{"Symbol":"CuSt","Name":"Copper, Stagnant","Value":".636","Unit":"mg/L"},"Fl":{"Symbol":"Fl","Name":"Fluoride","Value":".8","Unit":"mg/L"},"Har":{"Symbol":"Har","Name":"Hardness as CaCO3","Value":"34.1","Unit":"mg/L"},"Fe":{"Symbol":"Fe","Name":"Iron","Value":"0","Unit":"mg/L"},"Pb":{"Symbol":"Pb","Name":"Lead","Value":"0","Unit":"mg/L"},"PbSt":{"Symbol":"PbSt","Name":"Lead, Stagnant","Value":".010","Unit":"mg/L"},"Mn":{"Symbol":"Mn","Name":"Manganese","Value":"0","Unit":"mg/L"},"NO3":{"Symbol":"NO3","Name":"Nitrate-N","Value":".99","Unit":"mg/L"},"NO2":{"Symbol":"NO2","Name":"Nitrite-N","Value":"0","Unit":"mg/L"},"ph":{"Symbol":"ph","Name":"pH","Value":"6.62","Unit":"units"},"Na":{"Symbol":"Na","Name":"Sodium","Value":"9.24","Unit":"mg/L"}},"Bac_G":"rdb_Bac_False","Ecoli_G":"rdb_Ecoli_False","RadionuclideContaminants":{"Rn":{"Symbol":"Rn","Name":"Radon","Value":"2194","Unit":"pCi/L"},"U":{"Symbol":"U","Name":"Uranium","Value":"8","Unit":"μg/L"},"AGA":{"Symbol":"AGA","Name":"Gross Alpha","Value":"7.3","Unit":"pCi/L"}}};
+      /*sample = {
+        "CityName":"Anonymous",
+        "RoutineContaminants":{
+          "As":{
+            "Symbol":"As",
+            "Name":"Arsenic",
+            "Value":"1234",
+            "Unit":"mg/L"
+          },
+          "Cl":{
+            "Symbol":"Cl",
+            "Name":"Chloride",
+            "Value":"1234",
+            "Unit":"mg/L"
+          },
+          "Cu":{
+            "Symbol":"Cu",
+            "Name":"Copper",
+            "Value":"1234",
+            "Unit":"mg/L"
+          },
+          "CuSt":{
+            "Symbol":"CuSt",
+            "Name":"Copper, Stagnant",
+            "Value":"1234",
+            "Unit":"mg/L"
+          },
+          "Fl":{
+            "Symbol":"Fl",
+            "Name":"Fluoride",
+            "Value":"1234",
+            "Unit":"mg/L"
+          },
+          "Har":{
+            "Symbol":"Har",
+            "Name":"Hardness as CaCO3",
+            "Value":"1234",
+            "Unit":"mg/L"
+          },
+          "Fe":{
+            "Symbol":"Fe",
+            "Name":"Iron",
+            "Value":"1234",
+            "Unit":"mg/L"
+          },
+          "Pb":{
+            "Symbol":"Pb",
+            "Name":"Lead",
+            "Value":"1234",
+            "Unit":"mg/L"
+          },
+          "PbSt":{
+            "Symbol":"PbSt",
+            "Name":"Lead, Stagnant",
+            "Value":"1234",
+            "Unit":"mg/L"
+          },
+          "Mn":{
+            "Symbol":"Mn",
+            "Name":"Manganese",
+            "Value":"1234",
+            "Unit":"mg/L"
+          },
+          "NO3":{
+            "Symbol":"NO3",
+            "Name":"Nitrate-N",
+            "Value":"1234",
+            "Unit":"mg/L"
+          },
+          "NO2":{
+            "Symbol":"NO2",
+            "Name":"Nitrite-N",
+            "Value":"1234",
+            "Unit":"mg/L"
+          },
+          "ph":{
+            "Symbol":"ph",
+            "Name":"pH",
+            "Value":"6.1",
+            "Unit":"units"
+          },
+          "Na":{
+            "Symbol":"Na",
+            "Name":"Sodium",
+            "Value":"1234",
+            "Unit":"mg/L"
+          }
+        },
+        "BacterialContaminants":{
+          "Bac":{
+            "Symbol":"Bac",
+            "Name":"Total Coliform",
+            "Value":"1234",
+            "Unit":"CFU/100 mL"
+          },
+          "Ecoli":{
+            "Symbol":"Ecoli",
+            "Name":"E. Coli",
+            "Value":"1234",
+            "Unit":"CFU/100 mL"
+          }
+        },
+        "Bac_G":"rdb_Bac_True",
+        "Ecoli_G":"rdb_Ecoli_True",
+        "RadionuclideContaminants":{
+          "Rn":{
+            "Symbol":"Rn",
+            "Name":"Radon",
+            "Value":"1234",
+            "Unit":"pCi/L"
+          },
+          "U":{
+            "Symbol":"U",
+            "Name":"Uranium",
+            "Value":"1234",
+            "Unit":"μg/L"
+          },
+          "AGA":{
+            "Symbol":"AGA",
+            "Name":"Gross Alpha",
+            "Value":"1234",
+            "Unit":"pCi/L"
+          }
+        }
+      };*/
     }
 
     for(var cat in sample){
@@ -198,11 +323,12 @@ function resetBWIForm() {
     var $results_wrapper = $('#be-well-informed-results-wrapper');
     var $all_wrappers = $('.be-well-informed-modal-wrapper');
     var formData = $form.serializeObject();
+    var data = Object.keys(formData).reduce(checkValues, formData);
     showElementOutOfMany($loading_wrapper, $all_wrappers);
     $.ajax({
       url: 'be_well_informed/form_submission',
       method: 'POST',
-      data: Object.keys(formData).reduce(checkValues, formData),
+      data: data,
       success: function(be_well_response_json) {
         if (!be_well_response_json.error) {
           console.log(be_well_response_json.data);
