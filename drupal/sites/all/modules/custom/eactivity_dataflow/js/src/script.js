2e55c3198127fd0f757a9376f31c9afa6ccfb0d6
smolinsk <samuel.molinski@cgifederal.com>
2016-10-04 13:37:26 -0500

    EE-1552: added some css

--- drupal/sites/all/modules/custom/eactivity_dataflow/js/src/script.js
+++ drupal/sites/all/modules/custom/eactivity_dataflow/js/src/script.js
@@ -8,12 +8,12 @@
           var $refreshLink = $(this);
           $refreshLink.after('<div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div> Refreshing...</div>');
 
+          $('#gridstack-pane-views-to_do-block_1').find('div.view').addClass('loading')
           // 'todo/refresh' calls refresh_todo_list() in eenterprise_bridge_auth.pages.inc
           $.get('todo/refresh', function() {
             console.log('ajax complete, refresh the ctools forms to refresh the view');
             $refreshLink.parent().triggerHandler('RefreshView');
             $refreshLink.next('div.ajax-progress.ajax-progress-throbber').remove();
-            $refreshLink.parent().find('div.view').first().children('div, ul').show();
           });
           return false;
         });
@@ -24,8 +24,8 @@
   // @see http://stackoverflow.com/questions/5641798/load-javascript-after-page-is-fully-rendered
   $(window).load(function() {
     // TODO: hide using CSS?
-    $('#gridstack-pane-views-to_do-block_1').find('div.view').first().children('div, ul').hide();
-    $('#gridstack-pane-views-progress_tracker-block_1').find('div.view').first().children('div, ul').hide();
+    $('#gridstack-pane-views-to_do-block_1').find('div.view').first().addClass('loading');
+    $('#gridstack-pane-views-progress_tracker-block_1').find('div.view').first().addClass('loading');
 
     $('#gridstack-pane-views-to_do-block_1').find('a.refresh').click();
     $('#gridstack-pane-views-progress_tracker-block_1').find('a.refresh').click();
