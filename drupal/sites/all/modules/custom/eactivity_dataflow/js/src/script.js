94f4ea98602a382c84a818cc784ab64b8e8fbc6d
smolinsk <samuel.molinski@cgifederal.com>
2016-10-04 14:11:20 -0500

    EE-1552: added some css and modified the appearance to be inline with what CM requested

--- drupal/sites/all/modules/custom/eactivity_dataflow/js/src/script.js
+++ drupal/sites/all/modules/custom/eactivity_dataflow/js/src/script.js
@@ -4,14 +4,12 @@
     attach: function (context) {
       $('body').once('widget-refresh-by-ajax', function () {
         $('#gridstack-pane-views-to_do-block_1, #gridstack-pane-views-progress_tracker-block_1').on('click', 'a.refresh', function(ev) {
-          console.log('do not load link, perform ajax instead');
           var $refreshLink = $(this);
+          $refreshLink.parent().addClass('loading')
           $refreshLink.after('<div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div> Refreshing...</div>');
 
-          $('#gridstack-pane-views-to_do-block_1').find('div.view').addClass('loading')
           // 'todo/refresh' calls refresh_todo_list() in eenterprise_bridge_auth.pages.inc
           $.get('todo/refresh', function() {
-            console.log('ajax complete, refresh the ctools forms to refresh the view');
             $refreshLink.parent().triggerHandler('RefreshView');
             $refreshLink.next('div.ajax-progress.ajax-progress-throbber').remove();
           });
