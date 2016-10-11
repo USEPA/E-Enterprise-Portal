9e53571253c041b6e925664013505929caf5ff6b
Christian Manalansan <christian.manalansan@cgifederal.com>
2016-10-11 08:38:58 -0400

    EE-1552 Only perform an initial refresh of [todo/progress tracker] if we have never done this before (e.g.: after logging in, but NOT after refreshing the page)
    Also, remove the status message

--- drupal/sites/all/modules/custom/eactivity_dataflow/js/src/script.js
+++ drupal/sites/all/modules/custom/eactivity_dataflow/js/src/script.js
@@ -21,11 +21,14 @@
 
   // @see http://stackoverflow.com/questions/5641798/load-javascript-after-page-is-fully-rendered
   $(window).load(function() {
-    // TODO: hide using CSS?
-    $('#gridstack-pane-views-to_do-block_1').find('div.view').first().addClass('loading');
-    $('#gridstack-pane-views-progress_tracker-block_1').find('div.view').first().addClass('loading');
+    // only perform an initial refresh if we have never done this before (e.g.: after logging in, but NOT after refreshing the page)
+    if (!(Drupal.settings.eactivity_dataflow.todo_refresh_count > 0)) {
+      // TODO: hide using CSS?
+      $('#gridstack-pane-views-to_do-block_1').find('div.view').first().addClass('loading');
+      $('#gridstack-pane-views-progress_tracker-block_1').find('div.view').first().addClass('loading');
 
-    $('#gridstack-pane-views-to_do-block_1').find('a.refresh').click();
-    $('#gridstack-pane-views-progress_tracker-block_1').find('a.refresh').click();
+      $('#gridstack-pane-views-to_do-block_1').find('a.refresh').click();
+      $('#gridstack-pane-views-progress_tracker-block_1').find('a.refresh').click();
+    }
   });
 })(jQuery);
