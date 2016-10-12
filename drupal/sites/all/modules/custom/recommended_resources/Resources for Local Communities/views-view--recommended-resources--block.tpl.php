2cd83eac0b2b54ef323516e660287053b5f12cfc
Christian Manalansan <christian.manalansan@cgifederal.com>
2016-10-12 13:49:23 -0400

    EE-1563 Move the business logic (code inside the <?php ?> tags) in a preprocess hook

--- drupal/sites/all/modules/custom/recommended_resources/Resources for Local Communities/views-view--recommended-resources--block.tpl.php
+++ drupal/sites/all/modules/custom/recommended_resources/Resources for Local Communities/views-view--recommended-resources--block.tpl.php
@@ -38,15 +38,7 @@ drupal_add_css(drupal_get_path('module', $module_name) . "/css/recommended_resou
 
 <a class="about-widget ee-bootstrap-tooltip favorites-ignore" id="resources-lgc-help" href="javascript:void(0)" title="Learn more about the Resources for Local Communities widget"><img src="<?php print base_path() . path_to_theme(); ?>/images/question-circle.svg" alt="Learn more about the Resources for Local Communities widget"></a>
 <div id="resources-lgc-about" class="element-hidden">
-<?php   
-  $path = "recommended_resources-about";
-  $org_path = drupal_lookup_path("source", $path);
-  $node = menu_get_object("node", 1, $org_path);
-  $nid=$node->nid;
-  $node = node_load($nid); 
-  $nodeView = node_view($node, 'full'); 
-  print drupal_render($nodeView['body']);
-?>  
+  <?php print $recommended_resources_html; ?>
 </div>
 <div class="back-to-lgc-widget">
   <div class="usa-grid">
