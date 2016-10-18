1b69a701fb1cba9cb4efc56c032ca53661061f1f
Kate Walser <kate@cxinsights.com>
2016-10-17 23:45:34 -0400

    SOR-2512, SOR-2513:  Chemical Laws and Regulations Widget, Modal * Adds referenceable containers for updating modal content

--- drupal/sites/all/modules/custom/chemical_rules/templates/chemical-rules-modal.tpl.php
+++ drupal/sites/all/modules/custom/chemical_rules/templates/chemical-rules-modal.tpl.php
@@ -1,11 +1,11 @@
 <div class="usa-grid-full">
   <div id="chemical-rules-loading-wrapper"
        class="chemical-rules-modal-wrapper">
-    <h2>Loading search results&hellip;</h2> <i class="fa fa-spinner" aria-hidden="true"></i>
+    <h2>Loading search results for <span id="user-chemical-name"></span>&hellip;</h2> <i class="fa fa-spinner" aria-hidden="true"></i>
   </div>
   <div id="chemical-rules-results-wrapper"
        class="chemical-rules-modal-wrapper">
-    <h1 id="cr-modal-title">Acetone</h1>
+    <h2 id="cr-modal-title">Acetone</h2>
     <ul class="cr-modal-toc" id="cr-modal-toc-icons">
       <li><a href="#cr-laws-regs">Laws & Regulations</a></li>
       <li><a href="#cr-programs">Programs</a></li>
@@ -16,9 +16,9 @@
     </ul>
     
     <!-- @LAWS AND REGULATIONS -->
-    <h2 id="cr-laws-regs">Laws &amp; Regulations</h3>
+    <h3 id="cr-laws-regs">Laws &amp; Regulations</h4>
     <!-- @TODO List laws and regulations for the Search term -->  
-    <h3>3 laws and regulations found for Acetone:</h3>
+    <h4>3 laws and regulations found for Acetone:</h4>
     <ul class="cr-rules-regs_exact">
       <li><a href="#">40 CFR &sect; 261.31 - Hazardous Waste from non-specific sources.</a></li>  
     </ul>
@@ -26,75 +26,92 @@
   <!-- @TODO - If CFR returned due to specific synonym is discernible, include the next items -->  
     <!-- @TODO - See previous @TODO - If synonyms will be returned, list laws and regulations for Synonym 1 -->
     <!--
-    <h3>Acetone is a synonym for 2-Propanone. <span class="cr-match-type">1 rule and regulation found for 2-Propanone:</span></h3>
+    <h4>Acetone is a synonym for 2-Propanone. <span class="cr-match-type">1 rule and regulation found for 2-Propanone:</span></h4>
     <ul class="cr-rules-regs_synonyms">
       <li><a href="#">40 CFR &sect; 261.31 - Hazardous Waste from non-specific sources.</a></li>  
     </ul>  
     -->
     <!-- @TODO - See previous @TODO - List laws and regulations for Synonym 2 -->
     <!--
-    <h3>3 laws and regulations found for Acetone:</h3>
+    <h4>3 laws and regulations found for Acetone:</h4>
     <ul class="cr-rules-regs_substance_synonyms">
       <li><a href="#">40 CFR &sect; 261.31 - Hazardous Waste from non-specific sources.</a></li>  
     </ul>    
     -->
     
     <!-- @TODO List laws and regulations for Substance List -->
-    <h3>Acetone appears on the following Substance Lists: <span class="cr-match-type">Volatile Organic Compounds - 2 laws and regulations apply:</span></h3>
-    <ul class="cr-rules-regs_lists">
-      <li><a href="#">40 CFR &sect; 261.31 - Hazardous Waste from non-specific sources.</a></li>  
-    </ul>
+    <div id="cr-laws-regs_substances">
+      <!-- h4, ul, li -->
+      <h4>Acetone appears on the following Substance Lists: <span class="cr-match-type">Volatile Organic Compounds - 2 laws and regulations apply:</span></h4>
+      <ul class="cr-rules-regs_lists">
+        <li><a href="#">40 CFR &sect; 261.31 - Hazardous Waste from non-specific sources.</a></li>  
+      </ul>
+    </div><!-- @end #cr-laws-regs_substances -->
     
     <!-- @PROGRAMS -->
-    <h2 id="cr-programs">Programs</h2>
-    <div class="cr-programs_container">
-      <ul>
-        <li><a href="#">US EPA Toxic Substances Control Act Program</a></li>
-      </ul>
-    </div><!-- @end .cr-programs_container -->
+    <div id="cr-laws-regs_programs">
+      <h3 id="cr-programs">Programs</h3>
+      <div class="cr-programs_container">
+        <ul>
+          <li><a href="#">US EPA Toxic Substances Control Act Program</a></li>
+        </ul>
+      </div><!-- @end .cr-programs_container -->
+    </div><!-- @end #cr-laws-regs_programs -->
 
-    <!-- @STRUCTURE -->    
-    <h2 id="cr-structure">Structure</h2>
-    <div class="cr-structure_container">
-      <div class="cr-structure_image">
-        <img src="<?php print base_path() . path_to_theme(); ?>/images/imgsrv.png" alt="A structure of acetone">
-        <p>Powered by <a href="https://pubchem.ncbi.nlm.nih.gov" rel="external" target="_blank">PubChem</a></p>
-      </div>
-      <div class="cr-structure_name">
-        <p>C3H6O</p>
-      </div>
-    </div><!-- @end .cr-structure_container -->
+    <!-- @STRUCTURE --> 
+    <div id="cr-laws-regs_structure">   
+      <h3 id="cr-structure">Structure</h3>
+      <div class="cr-structure_container">
+        <div class="cr-structure_image">
+          <img src="<?php print base_path() . path_to_theme(); ?>/images/imgsrv.png" alt="A structure of acetone">
+          <p>Powered by <a href="https://pubchem.ncbi.nlm.nih.gov" rel="external" target="_blank">PubChem</a></p>
+        </div>
+        <div class="cr-structure_name">
+          <p>C3H6O</p>
+        </div>
+      </div><!-- @end .cr-structure_container -->
+    </div><!-- @end #cr-laws-regs_structure -->
 
     <!-- @CHEMICAL & PHYSICAL PROPERTIES -->    
-    <h2 id="cr-properties">Programs</h2>
-    <div class="cr-properties_container">
-      <table>
-        <thead>
-          <tr>
-            <th>Property</th>
-            <th>Value</th>
-          </tr>
-        </thead>
-        <tbody>
-          
-      </table>
-    </div><!-- @end .cr-properties_container -->    
+    <div id="cr-laws-regs_properties">      
+      <h3 id="cr-properties">Chemical &amp; Physical Properties</h3>
+      <div class="cr-properties_container">
+        <table>
+          <thead>
+            <tr>
+              <th scope="col">Property</th>
+              <th scope="col">Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Text</th>
+              <td>Text</td>
+            </tr>
+          </tbody> 
+        </table>
+      </div><!-- @end .cr-properties_container -->    
+    </div><!-- @end #cr-laws-regs_properties -->    
 
     <!-- @SYNONYMS -->    
-    <h2 id="cr-synonyms">Synonyms</h2>
-    <div class="cr-synonyms_container">
-      <ul>
-        <li>Synonym 1</li>
-      </ul>
-    </div><!-- @end .cr-synonyms_container -->    
-
+    <div id="cr-laws-regs_synonyms">
+      <h3 id="cr-synonyms">Synonyms</h3>
+      <div class="cr-synonyms_container">
+        <ul>
+          <li>Synonym 1</li>
+        </ul>
+      </div><!-- @end .cr-synonyms_container -->    
+    </div><!-- @end #cr-laws-regs_synonyms -->    
+    
     <!-- @SUBSTANCE LISTS -->    
-    <h2 id="cr-lists">Substance Lists</h2>
-    <div class="cr-lists_container">
-      <ul>
-        <li>Substance List 1</li>
-      </ul>
-    </div><!-- @end .cr-lists_container -->    
-
+    <div id="cr-laws-regs_lists">    
+      <h3 id="cr-lists">Substance Lists</h3>
+      <div class="cr-lists_container">
+        <ul>
+          <li>Substance List 1</li>
+        </ul>
+      </div><!-- @end .cr-lists_container -->    
+    </div><!-- @end #cr-laws-regs_lists -->
+    
   </div><!-- @end .chemical-rules-results-wrapper -->
 </div><!-- @end usa-grid-full for CR -->
--- drupal/sites/all/modules/custom/chemical_rules/templates/chemical-rules.tpl.php
+++ drupal/sites/all/modules/custom/chemical_rules/templates/chemical-rules.tpl.php
@@ -4,39 +4,43 @@
 <p>
   Find federal laws and regulations.  As state and local partners join, more resources will be available.  <a href="javascript:void(0)" id="learn-more_chemical-rules">Learn how it works</a>.
 </p>
+<div id="cr-search">
+  <div id="cr-search_field">
+    <label for="cr-search_input">Enter chemical name or CAS #</label>
+    <input id="cr-search_input" name="cr-search_input" type="text" class="form-text" size="60" maxlength="128">
+  </div><!-- @end cr-search_field -->
+  <button id="cr-search-chems-btn">Search chemicals</button>  
+  <div id="cr-search_description" class="description">Powered by EPA <a href="https://epa.gov/srs">Substance Registry Service</a> and <a href="https://epa.gov/lrs">Laws & Regulations Service</a></div>
+</div><!-- @end cr-search -->    
 <div id="cr-tabs">
     <ul>
       <li><a class="favorites-ignore" href="#cr-tabs_favorites">My Favorites</a></li>
-      <li><a class="favorites-ignore" href="#cr-tabs_search">Search</a></li>
+      <li><a class="favorites-ignore" href="#cr-tabs_previous">Previous Searches</a></li>      
 <!--       <li><a class="favorites-ignore" href="#cr-tabs_trending">Trending</a></li> -->
     </ul>
     <!-- @TAB: My Favorites -->
-    <div id="cr-tabs_favorites" style="padding:0">
+    <div id="cr-tabs_favorites">
       <div class="cr-tabs_favorites_empty">
         <p>
           You have 0 favorite chemicals saved.  To add a favorite, use the <a href="#cr-tabs_search">search</a> option.
         </p>
       </div><!-- @end cr-tabs_favorites_empty -->
-      <div class="cr-tabs_favorites_available">
+      <div class="cr-tabs_favorites_available hidden">
         <! -- @TODO - List favorites -->
-        <ul class="cr-tabs_favorites-list">
-          <li><a class="cr-tabs_favorites-link" href="#">Acetone</a><a class="cr-tabs_favorites-remove">Remove</a></li> 
+        <ul class="cr-lists cr-tabs_favorites-list">
+          <li><a class="cr-tabs_favorites-link" href="#" data-epachemintnum="9999" data-systematicname="Acetone">67-64-1:  Acetone</a><a class="cr-tabs_favorites-remove">Remove</a></li> 
         </ul>      
       </div><!-- @end cr-tabs_favorites_empty -->
     </div><!-- @end cr-tabs_favorites -->
     <!-- @TAB: Search -->    
-    <div id="cr-tabs_search" style="padding:0">
+    <div id="cr-tabs_previous">
       <p>
-        Use the search below to find rules and regulations for chemicals.
+        Here are your last searches:
       </p>
-      <p>
-        <label for="cr-search">Enter chemical name or CAS #</label>
-        <input id="cr-search" name="cr-search" type="text" class="form-text" aria-describedby="cr-search_description" size="60" maxlength="128">
-        <div id="cr-search_description" class="description">Powered by EPA <a href="https://epa.gov/srs">Substance Registry Service</a> and <a href="https://epa.gov/lrs">Laws & Regulations Service</a></div>
-        <button id="cr-search-chems-btn">
-          Search chemicals
-        </button>
-      </p>
-    </div><!-- @end cr-tabs_search -->    
+      <ul class="cr-lists">
+        <li><a href="#" data-epachemintnum="9999" data-systematicname="Acetone">67-64-1:  Acetone</a></li> 
+        <li><a href="#" data-epachemintnum="9999" data-systematicname="2,4,4-Trimethylhexamethylene diisocyanate">16938-22-0:  2,4,4-Trimethylhexamethylene diisocyanate</a></li>         
+      </ul>  
+    </div><!-- @end cr-tabs_previous -->    
 </div><!-- @end cr-tabs -->
 <div id="chemical-rules-modal"></div>
