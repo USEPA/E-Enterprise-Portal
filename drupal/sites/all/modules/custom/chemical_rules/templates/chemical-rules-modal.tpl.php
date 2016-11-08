813fe86fdd79ee46f1f375aebae7f4b27d2e1838
Kate Walser <kate@cxinsights.com>
2016-11-08 11:38:50 -0500

    SOR-2573, SOR-2621, SOR-2623, SOR-2624: Chemical Laws and Regs Widget * Makes TOC icons sticky at top, adjusts size on scroll * Adjusts body div to be fixed so avoid scrolling background and modal * Shows Loading message while search results being retrieved * Sets dialog title to search results when searching and chemical name when click favorites * Shows common, systematic names and CAS number in dialog * Adds images for TOC icons

--- drupal/sites/all/modules/custom/chemical_rules/templates/chemical-rules-modal.tpl.php
+++ drupal/sites/all/modules/custom/chemical_rules/templates/chemical-rules-modal.tpl.php
@@ -1,7 +1,7 @@
 <div class="usa-grid-full">
   <div id="chemical-rules-loading-wrapper"
        class="chemical-rules-modal-wrapper">
-    <h1>Loading search results for <span class="cr-chemical-name"></span>&hellip;</h1> <i class="fa fa-spinner" aria-hidden="true"></i>
+    <h1>Loading search results for <span id="searching-chemical-name"></span>&hellip;</h1> <i class="fa fa-spinner" aria-hidden="true"></i>
   </div>
   <div id="chemical-rules-results-wrapper"
        class="chemical-rules-modal-wrapper">
