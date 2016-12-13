e36da710ad76d85ae2a1e58b2141064531d83e69
Kate Walser <kate@cxinsights.com>
2016-12-13 15:21:45 -0500

    EE-1674: Create CGP Widget Shell * Adds beginner module items

---
+++ drupal/sites/all/modules/custom/construction_permits/templates/construction-permits.tpl.php
@@ -0,0 +1,21 @@
+<p class="widget-note">Source: <a href="https://www3.epa.gov/" target="_blank" rel="external">US Environmental Protection Agency</a></p>
+<p>In areas where EPA is the permitting authority, regulated construction site operators are generally permitted under EPA’s 2012 Construction General Permit (CGP).  Find notices of intent, notices of termination, or low erosity waivers.</p>
+<h3>Enter one or more search criteria</h3>
+<form id="cgp-form">
+	<ul class="cgp-form-elements usa-grid">
+		<li class="usa-grid-one-whole"><label for="cgp-permit-number">Permit tracking number</label><input type="text" id="cgp-permit-number"></li>
+		<li class="usa-grid-one-half"><label for="cgp-permit-city">City</label><input type="text" id="cgp-permit-city"></li>
+		<li class="usa-grid-one-fourth"><label for="cgp-permit-state">State</label><input type="text" id="cgp-permit-state"></li>
+		<li class="usa-grid-one-fourth"><label for="cgp-permit-zip">Zip</label><input type="text" id="cgp-permit-zip"></li>
+	</ul>
+	<h3><a href="#" class="favorites-ignore"><img src="sites/all/modules/custom/construction_permits/images/arrow-right.png" class="off" alt="Advanced search criteria collapsed. Click to expand"></a> Advanced search criteria</h3>
+	<div id="cgp-advanced-elements">
+		
+	</div>
+	<div id="cgp-form-buttons">
+	<div><a id="cgp-reset-button" href="javascript:void(0);" class="usa-button-outline">Reset</a>
+	<a id="cgp-search-button" href="javascript:void(0)" class="usa-button">Search</a></div>
+	</div>
+</form>
+<div id="construction-permits-modal"></div>
+<div id="construction-permits-learnmore"><?php print $cgp_learnmore; ?></div>
