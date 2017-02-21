cbbd091c2607e27a0e3398ab42320a5f203c2373
Kate Walser <kate@cxinsights.com>
2017-02-21 10:28:53 -0500

    EE-1787: BWI Missing Treatment * Hides rows from Summary table (only) when user did not enter value to match NH BWI and reduce scrolling * Adjusts size of icons down to reduce scrolling

--- drupal/sites/all/modules/custom/be_well_informed/js/src/be_well_informed.js
+++ drupal/sites/all/modules/custom/be_well_informed/js/src/be_well_informed.js
@@ -413,6 +413,7 @@ function resetBWIForm() {
 
             $table = $('<table id="be-well-informed-results-table" class="eportal-responsive-table usa-table-borderless"> <thead> <tr> <th>Result</th> <th>Element</th> <th>Your Entry</th> <th>Limit</th> <th>About Your Well Water</th> </tr> </thead> <tbody></tbody> </table>')
             $table.appendTo('.be-well-informed-results').DataTable(default_datatable_result_summary_options);
+            $table.find('.bwi-hide-row').closest('tr').remove();
             $table.find('td:contains("Total Coliform (Bac)")').text('Total Coliform');  // Remove Bac symbol
 
             $table = $('<table id="be-well-informed-result-details-table" class="eportal-responsive-table usa-table-borderless"> <thead> <tr> <th>Result</th> <th>Element</th> <th>Your Entry</th> <th>Limit</th> <th>About Your Well Water</th> </tr> </thead> <tbody></tbody> </table>')
