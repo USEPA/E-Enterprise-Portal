8fc1bc2e99f8be4faebef890b4018411dd828312
smolinsk <samuel.molinski@cgifederal.com>
2017-05-04 10:48:54 -0500

    for the new BWI work

---
+++ drupal/sites/all/modules/custom/be_well_informed/templates/be-well-informed-modal-state-selection.tpl.php
@@ -0,0 +1,87 @@
+<div class="usa-grid-full">
+    <p>Select Your State/Tribe:</p>
+    <div id="bwi-state-selection" class="ee-grid"
+         data-parsley-validate="" novalidate="">
+        <div class="form-elements">
+            <div class="line">
+                <div class="col-md-12">
+                    <select id="bwi-state" name="bwi-state">
+                        <!--<option value="AL">Alabama</option>
+                        <option value="AK">Alaska</option>
+                        <option value="AS">American Samoa</option>
+                        <option value="AZ">Arizona</option>
+                        <option value="AR">Arkansas</option>
+                        <option value="AT">Atlantic Offshore</option>
+                        <option value="CA">California</option>
+                        <option value="CO">Colorado</option>
+                        <option value="CT">Connecticut</option>
+                        <option value="DE">Delaware</option>
+                        <option value="DC">District Of Columbia</option>
+                        <option value="FM">Federated States Of Micronesia
+                        </option>
+                        <option value="FL">Florida</option>
+                        <option value="GA">Georgia</option>
+                        <option value="GU">Guam</option>
+                        <option value="GM">Gulf Of Mexico</option>
+                        <option value="GE">Gulf of Mexico East</option>
+                        <option value="HI">Hawaii</option>
+                        <option value="ID">Idaho</option>
+                        <option value="IL">Illinois</option>
+                        <option value="IN">Indiana</option>
+                        <option value="IA">Iowa</option>
+                        <option value="JA">Johnston Atoll</option>
+                        <option value="KS">Kansas</option>
+                        <option value="KY">Kentucky</option>
+                        <option value="LA">Louisiana</option>
+                        <option value="ME">Maine</option>
+                        <option value="MD">Maryland</option>-->
+                        <option value="MA">Massachusetts</option>
+                        <!--<option value="MI">Michigan</option>
+                        <option value="MW">Midway Island and Wake Island
+                        </option>
+                        <option value="MN">Minnesota</option>
+                        <option value="MS">Mississippi</option>
+                        <option value="MO">Missouri</option>
+                        <option value="MT">Montana</option>
+                        <option value="NE">Nebraska</option>
+                        <option value="NV">Nevada</option>-->
+                        <option value="NH">New Hampshire</option>
+                        <!--<option value="NJ">New Jersey</option>
+                        <option value="NM">New Mexico</option>
+                        <option value="NY">New York</option>
+                        <option value="NC">North Carolina</option>
+                        <option value="ND">North Dakota</option>
+                        <option value="MP">Northern Mariana Islands</option>
+                        <option value="OH">Ohio</option>
+                        <option value="OK">Oklahoma</option>
+                        <option value="OR">Oregon</option>
+                        <option value="PA">Pennsylvania</option>
+                        <option value="PR">Puerto Rico</option>
+                        <option value="MH">Republic Of Marshall Islands</option>
+                        <option value="PW">Republic Of Palau</option>
+                        <option value="RI">Rhode Island</option>
+                        <option value="SC">South Carolina</option>
+                        <option value="SD">South Dakota</option>
+                        <option value="TN">Tennessee</option>
+                        <option value="TX">Texas</option>
+                        <option value="UT">Utah</option>
+                        <option value="VT">Vermont</option>
+                        <option value="VI">Virgin Islands</option>
+                        <option value="VA">Virginia</option>
+                        <option value="WA">Washington</option>
+                        <option value="WV">West Virginia</option>
+                        <option value="WI">Wisconsin</option>
+                        <option value="WY">Wyoming</option>-->
+                    </select>
+                </div>
+            </div>
+            <div id="bwi-form-buttons" class="col-md-12">
+                <p class="text-right">
+                    <button id="bwi-state-confirm-button" type="button"
+                            class="usa-button">Confirm
+                    </button>
+                </p>
+            </div>
+        </div>
+    </div>
+</div><!-- @end .usa-grid-full -->
