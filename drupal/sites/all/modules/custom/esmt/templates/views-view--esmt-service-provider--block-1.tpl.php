33fa7e8617f97f09fbb9fae9951dad8d00e48598
Matkin, Benjamin K (CGI Federal) <benjamin.matkin@cgifederal.com>
2017-11-14 13:31:08 -0600

    Limit gridstack being added on any other page than workbench. Alter View to use custom tpl for faster MVP.

---
+++ drupal/sites/all/modules/custom/esmt/templates/views-view--esmt-service-provider--block-1.tpl.php
@@ -0,0 +1,5 @@
+<table class="table eportal-responsive-table">
+  <?php foreach($service_providers as $service_provider): ?>
+    <tr><td><?php print $service_provider; ?></td></tr>
+  <?php endforeach; ?>
+</table>
