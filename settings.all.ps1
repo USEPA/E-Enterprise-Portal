01dfb271713628e3aaea83af2118fc95b8431f7c
Molinski, Samuel J (CGI Federal) <samuel.molinski@cgifederal.com>
2018-10-18 08:17:59 -0500

    - Create Powershell scripts to help with file transfer

---
+++ settings.all.ps1
@@ -0,0 +1,5 @@
+$src = "./"
+$dest = './test'
+$excluded_files = 'excluded_files.all.txt'
+$excluded_folders = 'excluded_folders.all.txt'
+$vendor_folders = 'drupal/vendor app/node_modules drupal/web/core drupal/web/drivers'
