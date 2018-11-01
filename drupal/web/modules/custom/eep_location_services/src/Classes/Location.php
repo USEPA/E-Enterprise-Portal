77c988e52bfa324ba45fadba421d9dba73a21dba
Beach, Richard H (CGI Federal) <richard.h.beach@cgifederal.com>
2018-11-01 10:29:42 -0500

    got endpoint working

---
+++ drupal/web/modules/custom/eep_location_services/src/Classes/Location.php
@@ -0,0 +1,13 @@
+<?php
+
+class Location {
+  /**
+   * Global variables that make up a location
+   */
+  private $zipcode;
+  private $city;
+  private $state;
+  private $latitude;
+  private $longitude;
+
+}
