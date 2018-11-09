5d7250e352a9ecc0692ccae415aa3fb365d26c26
Beach, Richard H (CGI Federal) <richard.h.beach@cgifederal.com>
2018-11-09 12:28:14 -0600

    updating http request

--- app/src/components/LocationSearch.vue
+++ app/src/components/LocationSearch.vue
@@ -14,10 +14,8 @@
 </template>
 
 <script>
-    import {mapActions} from 'vuex';
-    import {mapGetters} from 'vuex';
+    import {mapActions, mapGetters} from 'vuex';
 
-    const name = "LocationSearch"
     export default{
         data: function() {
             return{
@@ -29,9 +27,8 @@
                         'createLocationRequest'
                     ]
             ),
-            submitLocation(event){
+            submitLocation(){
                 alert(this.location);
-
             }
         }
     }
