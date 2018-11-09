83a87bf6b2681c0c2a26e068ab7dbc1b76a668f5
Beach, Richard H (CGI Federal) <richard.h.beach@cgifederal.com>
2018-11-09 12:28:35 -0600

    updating http request

--- app/src/store/actions.js
+++ app/src/store/actions.js
@@ -3,6 +3,6 @@ import { AppAxios, commonAppStore } from '../modules/wadk/WADK';
 export default {
   ...commonAppStore.actions,
   createLocationRequest(context) {
-    alert("create Location Request action rendered");
+    alert('create Location Request action rendered');
   },
 };
