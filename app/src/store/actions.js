234b1eaa554ac2afcbf585bceb91e3bff21ba8bd
Molinski, Samuel J (CGI Federal) <samuel.molinski@cgifederal.com>
2018-11-13 12:53:10 -0600

    - wired up the modal

--- app/src/store/actions.js
+++ app/src/store/actions.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import { AppAxios, commonAppStore } from '../modules/wadk/WADK';
+import { EventBus } from '../EventBus';
 
 export default {
   ...commonAppStore.actions,
   createLocationRequest(context, location) {
     // Variable declerations
+    const rootStore = this;
+    const env = rootStore.getters.getEnvironment;
     const store = context;
     const userLocation = {
       zipcode: '',
@@ -16,12 +19,12 @@ export default {
 
     // Input validation for URL formation
     if (/^\d{5}(-\d{4})?$/.test(location)) {
-      //url += 'zipcode=' + location;
+      // url += 'zipcode=' + location;
       // @todo clean string/trim
       userLocation.zipcode = location;
     } else if (/([A-Za-z]+(?: [A-Za-z]+)*),? ([A-Za-z]{2})/.test(location)
       || /([A-Za-z]+(?: [A-Za-z]+)*),?([A-Za-z]{2})/.test(location)) {
-      const decoupled_location = location.split(",");
+      const decoupled_location = location.split(',');
       const city = decoupled_location[0].toUpperCase().trim();
       const state = decoupled_location[1].toUpperCase().trim();
       // url += 'city=' +
@@ -34,12 +37,16 @@ export default {
 
     // Make the request to retrieve the correct location information
     AppAxios.get(url).then((response) => {
-      const data = response.data;
+      const { data } = response;
       console.log(data);
-      this.$modals.show('location-search-modal');
+
+      EventBus.$emit('locationSearch::showUserConfirmationModal', {
+        callee: this,
+        value: data,
+      });
 
       // // @todo figure out if has multiple options and ask for user input
-      store.commit('SET_USER_LOCATION', userLocation);
+      // store.commit('SET_USER_LOCATION', userLocation);
     }).catch((...args) => {
       // @todo add sanity check for errors & visual prompt to the user
       app.$Progress.fail();
