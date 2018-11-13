4b40b28be6d56c1bbddf83b870b9287a79855a94
Molinski, Samuel J (CGI Federal) <samuel.molinski@cgifederal.com>
2018-11-13 13:05:50 -0600

    added payload variable

--- app/src/components/LocationSearch.vue
+++ app/src/components/LocationSearch.vue
@@ -1,23 +1,35 @@
 <!-- get the button call back to call the axios function in actions.js-->
 <template>
-    <div id="location-wrapper" class="col-6">
-        <b-input-group>
-            <b-form-input id="location-input" type="text" v-model="location"
-                          placeholder="Enter city, state; or ZIP code"></b-form-input>
-            <b-input-group-append>
-                <b-button id="submit-location" v-on:click="submitLocation">
-                    <div class="button-image">
-                        <img id="go-btn-image" src="../assets/images/go_button.png" alt="Go">
-                    </div> <!-- @TODO: make the background image an arrow get from kate -->
-                </b-button>
-            </b-input-group-append>
-        </b-input-group>
-        <AppModal v-if="true" id="location-search-modal-interactive"
-                  modal-ref="location-search-modal"
-                  title="Additional Information Needed">
-            this is the app modal
-        </AppModal>
-    </div>
+  <div
+    id="location-wrapper"
+    class="col-6">
+    <b-input-group>
+      <b-form-input
+        id="location-input"
+        type="text"
+        v-model="location"
+        placeholder="Enter city, state; or ZIP code"/>
+      <b-input-group-append>
+        <b-button
+          id="submit-location"
+          @click="submitLocation">
+          <div class="button-image">
+            <img
+              id="go-btn-image"
+              src=""
+              alt="Go">
+          </div> <!-- @TODO: make the background image an arrow get from kate -->
+        </b-button>
+      </b-input-group-append>
+    </b-input-group>
+    <AppModal
+      v-if="true"
+      id="location-search-modal-interactive"
+      modal-ref="location-search-modal-interactive"
+      title="Additional Information Needed">
+      this is the app modal
+    </AppModal>
+  </div>
 </template>
 
 <script>
@@ -38,7 +50,6 @@
       };
     },
     created() {
-
       // Custom event listeners
       EventBus.$on('locationSearch::showUserConfirmationModal', this.showUserConfirmationModal);
     },
@@ -47,7 +58,7 @@
       submitLocation() {
         this.$store.dispatch('createLocationRequest', this.location);
       },
-      showUserConfirmationModal() {
+      showUserConfirmationModal(payload) {
         const vm = this;
         const lsModal = vm.$refs.location_search_modal_interactive;
 
