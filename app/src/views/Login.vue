421d0f8ee4b19c93ecd1f64a6b6ebfd8e58829bd
Beach, Richard H (CGI Federal) <richard.h.beach@cgifederal.com>
2018-11-29 14:53:05 -0600

    Merge branch 'development' of https://alm.cgifederal.com/scm/scm/epacdx/e-enterprise-2.0 into feature/EE-2810-user-can-see-location-title-they
    # Conflicts:
    #	app/src/modules/TrendingAir/store/state.js

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -1,7 +1,7 @@
 <!-- Go through this folder and rename all of the occurances to be specific to the app that you are building -->
 <template>
     <div>
-        <h3>Login Page</h3>
+        <h3>Login</h3>
         <div class="login-btns pt-4">
             <a class="btn btn-sm btn-outline-secondary" :href="bridgeURL">Login with Exchange Network</a>
         </div>
