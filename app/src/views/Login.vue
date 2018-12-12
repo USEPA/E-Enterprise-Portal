bb8079d9a4c53a06d4256f56c8eb4be5035639bd
hkunatha <himabindu.kunatha@cgifederal.com>
2018-12-12 11:04:59 -0600

    added new tryit link in the header

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -1,10 +1,13 @@
 <!-- Go through this folder and rename all of the occurances to be specific to the app that you are building -->
 <template>
     <div>
-        <h3>Login</h3>
-        <div class="login-btns pt-4">
-            <a class="btn btn-sm btn-outline-secondary" :href="bridgeURL">Exchange Network</a>
-        </div>
+        <div>
+            <h1><strong>Log In </strong></h1>
+            <h6>Select your preferred login method to access E-Enterprise using one of your existing accounts. Choose a preferred login type and option to get started.</h6>
+    </div>
+    <div class="login-btns pt-4">
+        <a class="btn btn-sm btn-outline-secondary" :href="bridgeURL">Exchange Network</a>
+    </div>
     </div>
 </template>
 
