ff1451b5ff2efabc5788db9627d744c6f1a8f5d2
Beach, Richard H (CGI Federal) <richard.h.beach@cgifederal.com>
2018-12-26 10:14:06 -0500

    changed code spacing

--- app/src/views/User.vue
+++ app/src/views/User.vue
@@ -5,11 +5,11 @@
             <h3>User Information</h3>
             <div class="first-name-container">
                 <div>First Name</div>
-                <b-form-input id="first-name-input" type="text"></b-form-input>
+                <b-form-input id="first-name-input" type="text" disabled></b-form-input>
             </div>
             <div class="email-container pt-3">
                 <div>Email</div>
-                <b-form-input id="email-input" type="text"></b-form-input>
+                <b-form-input id="email-input" type="text" disabled></b-form-input>
             </div>
         </div>
     </div>
