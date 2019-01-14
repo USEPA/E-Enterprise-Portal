8ce53962b444b9dafad79a61b403ab8d12ccedf9
hkunatha <himabindu.kunatha@cgifederal.com>
2019-01-14 15:33:10 -0600

    Merge branch 'development' of https://alm.cgifederal.com/scm/scm/epacdx/e-enterprise-2.0 into feature/EE-2842-login-page-epa-cdx-user
    # Conflicts:
    #	app/public/sample_data/footer.json
    #	app/src/store/actions.js
    #	drupal/config/sync/rest.resource.entity.node.yml

--- app/src/views/User.vue
+++ app/src/views/User.vue
@@ -1,7 +1,14 @@
 <!--this view is so that users can find their information and view it and in the future, edit it-->
 <template>
-    <div class="user-info-container col-md-12" v-if="isLoggedIn">
-        <div class="information-container col-md-6">
+<div class="container">
+        <h3>Profile</h3>
+<b-card>
+<b-tabs class="profile-tabs" >
+ <b-tab title="Account" active>
+<b-container class="bv-example-row ml-2">
+    <div class="user-info-container col-md-12" >
+        <div class="information-container col-md-4">
+        <div></div>
             <h3>User Information</h3>
             <div class="first-name-container">
                 <div>First Name</div>
@@ -10,9 +17,16 @@
             <div class="email-container pt-3">
                 <div>Email</div>
                 <b-form-input id="email-input" v-model="email" type="text" disabled></b-form-input>
+
             </div>
         </div>
+        <p>All unsaved data will be lost upon navigating away from the Profile page</p>
     </div>
+    </b-container>
+    </b-tab>
+    </b-tabs>
+    </b-card>
+      </div>
 </template>
 
 <script>
