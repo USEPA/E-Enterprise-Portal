9bbbbba5d0ab4981b4487a94d59ce7a13caa5687
RBEACH <richard.h.beach@cgi.com>
2019-01-15 10:29:58 -0500

    Merge branch 'development' of https://alm.cgifederal.com/scm/scm/epacdx/e-enterprise-2.0 into EE-2948-fix-hardcoded-facebook-button-to
    # Conflicts:
    #	app/src/store/actions.js
    #	app/src/store/mutations.js
    #	app/src/store/types.js

--- drupal/web/modules/custom/api_login/src/Plugin/rest/resource/LoginResource.php
+++ drupal/web/modules/custom/api_login/src/Plugin/rest/resource/LoginResource.php
@@ -43,26 +43,26 @@ class LoginResource extends ResourceBase
         // Fetch all of the nodes by the node ids
         $nodes = \Drupal\node\Entity\Node::loadMultiple($node_ids);
 
+        // Query Drupal to get all terms for given vocabulary
+        $terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree('authentication_category');
+
 
         // Loop through the nodes to format the json out properly
         foreach ($nodes as $node) {
+            $target_id = $node->field_authentication_category[0]->get('target_id')->getValue();
             $formatted_node_array[] = [
                 'title' => $node->title,
                 'image_path' => $node->field_image_path,
                 'urn' => $node->field_urn,
-                'tax_category' => $this->get_taxonomy_for_given_id($node->field_authentication_category[0]->get('target_id')->getValue()),
+                'tax_category' => $this->get_taxonomy_for_given_id($target_id, $terms),
             ];
         }
         return new ResourceResponse(['message' => $formatted_node_array]);
     }
 
-    private function get_taxonomy_for_given_id($node_tax_id){
+    private function get_taxonomy_for_given_id($node_tax_id, $terms){
         // Declare variables
         $associated_term = "";
-
-        // @TODO remove this query from inside the loop
-        // Query Drupal to get all terms for given vocabulary
-        $terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree('authentication_category');
 
         // Loop through all terms to find the right name for the given id
         foreach ($terms as $term){
