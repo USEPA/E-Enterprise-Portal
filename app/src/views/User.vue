a978f5bf93810570417f25f6f0c468a1c16df448
mculpepp <michael.culpepper@cgifederal.com>
2019-01-23 10:31:47 -0600

    created Locations tab skeleton

--- app/src/views/User.vue
+++ app/src/views/User.vue
@@ -1,96 +1,147 @@
 <!--this view is so that users can find their information and view it and in the future, edit it-->
 <template>
-<div class="container" >
-        <h3>Profile</h3>
-<b-card>
-    <b-tabs class="profile-tabs" >
-        <b-tab title="Account" active>
-            <b-container class="bv-example-row ml-2">
-    <div class="user-info-container col-md-12" >
-        <div class="information-container col-md-4">
-        <div></div>
-            <h3>User Information</h3>
-            <div class="user-name-container">
-                <div>User Name </div>
-                <b-form-input id="first-name-input" v-model="username" type="text" disabled></b-form-input>
-            </div>
-            <div class="email-container pt-3">
-                <div>Email Address</div>
-                <b-form-input id="email-input" v-model="mail" type="text" disabled></b-form-input>
-             </div>
-        </div>
+  <div class="container" >
+    <h3>Profile</h3>
+    <b-card>
+      <b-tabs class="profile-tabs" >
 
-    </div>
-            </b-container>
+        <b-tab
+          title="Account"
+          active>
+          <b-container class="bv-example-row ml-2">
+            <div class="user-info-container col-md-12" >
+              <div class="information-container col-md-4">
+                <div/>
+                <h3>User Information</h3>
+                <div class="user-name-container">
+                  <div>User Name </div>
+                  <b-form-input
+                    id="first-name-input"
+                    v-model="username"
+                    type="text"
+                    disabled/>
+                </div>
+                <div class="email-container pt-3">
+                  <div>Email Address</div>
+                  <b-form-input
+                    id="email-input"
+                    v-model="mail"
+                    type="text"
+                    disabled/>
+                </div>
+              </div>
+            </div>
+          </b-container>
         </b-tab>
 
-                <b-tab title="Interest" active>
-                    <b-container class="bv-example-row ml-2">
-            <div class="interest-container " >
-                <div class="int-container ">
-                <div ></div>
-                    <h3>Interests</h3>
-                    <div class=" pt-3 d-flex">
-                    <div><h6>Organization</h6>
-                      <b-dropdown id="org_ddown1" text="Business" variant="outline-secondary" class="col-4-md organisation  mr-3" >
-
-                      </b-dropdown>
-                    </div>
-                    <div text="Role"><h6> Role</h6>
-                      <b-dropdown id="role_ddown2" text="Budget and operations"  variant="outline-secondary" class="col-4-md role mr-3">
-
-                      </b-dropdown>
-                    </div>
-                    </div>
+        <b-tab
+          title="Locations">
+          <b-container class="bv-example-row ml-2">
+            <div class="locations-container " >
+              <div class="loc-container ">
+                <h3 class="mt-2">Locations of Interest</h3>
+                <div class=" pt-3 d-flex">
+                  <p>Add your locations of interest to see environmental information relevant to
+                  those areas.
+                    <span class="font-weight-bold"> Click the ☆ icon next to a location to make it
+                    your default location.</span>
+                  </p>
                 </div>
-
+                <div class=" pt-3 d-flex">
+                  <p>Until a location is specified, the default location is set to
+                  Durham, North Carolina.</p>
+                </div>
+                <div class="pt-3 d-flex">
+                  <b-input-group>
+                    <label
+                      class="col-12 font-weight-bold"
+                    >
+                      Enter city, state; tribe; or ZIP code
+                    </label>
+                    <b-form-input
+                      class="col-6 ml-3"
+                      value="Durham, North Carolina">
+                    </b-form-input>
+                    <div class="col-6 cursor-pointer"><h4>☆</h4></div>
+                  </b-input-group>
+                </div>
+              </div>
             </div>
-                    </b-container>
-                </b-tab>
-          <p>All unsaved data will be lost upon navigating away from the Profile page</p>
-    </b-tabs>
-</b-card>
-      </div>
+          </b-container>
+        </b-tab>
+
+        <b-tab
+          title="Interest">
+          <b-container class="bv-example-row ml-2">
+            <div class="interest-container " >
+              <div class="int-container ">
+                <div />
+                <h3>Interests</h3>
+                <div class=" pt-3 d-flex">
+                  <div><h6>Organization</h6>
+                    <b-dropdown
+                      id="org_ddown1"
+                      text="Business"
+                      variant="outline-secondary"
+                      class="col-4-md organisation  mr-3" />
+                  </div>
+                  <div text="Role"><h6> Role</h6>
+                    <b-dropdown
+                      id="role_ddown2"
+                      text="Budget and operations"
+                      variant="outline-secondary"
+                      class="col-4-md role mr-3"/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </b-container>
+        </b-tab>
+
+        <p class="ml-4 mt-4">All unsaved data will be lost upon navigating
+        away from the Profile page.</p>
+      </b-tabs>
+    </b-card>
+  </div>
 </template>
 
 <script>
-    import { mapActions, mapGetters } from 'vuex';
+  import { mapActions, mapGetters } from 'vuex';
 
-    const moduleName = 'User';
+  const moduleName = 'User';
 
-    export default {
-        name: moduleName,
-        components: {
-        },
-        beforeCreate(){
+  export default {
+    name: moduleName,
+    components: {
+    },
+    beforeCreate() {
 
-        },
-        created(){
-            const store = this.$store;
-            if (!(store && store.state && store.state[moduleName])) {
-                store.registerModule(moduleName, storeModule);
-            }
-        },
-        data(){
-            return{
+    },
+    created() {
+      const store = this.$store;
+      if (!(store && store.state && store.state[moduleName])) {
+        store.registerModule(moduleName, storeModule);
+      }
+    },
+    data() {
+      return {
+        locations: [{ }],
+      };
+    },
 
-
-            }
-        },
-
-        computed:{
-            ...mapGetters({
-                // map getters go here
-                isLoggedIn: 'getIsLoggedIn',
-                username: 'getUsername',
-            }),
-        },
-        methods: {
-            ...mapActions(moduleName, [
-                // map actions go here
-            ]),
-        }
-    }
+    computed: {
+      ...mapGetters({
+        // map getters go here
+        isLoggedIn: 'getIsLoggedIn',
+        username: 'getUsername',
+      }),
+    },
+    methods: {
+      ...mapActions(moduleName, [
+        // map actions go here
+      ]),
+    },
+  };
 </script>
 
 <style scoped>
