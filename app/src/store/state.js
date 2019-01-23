e796ece7b32d500bcb8503b6cffa453a423850e2
RBEACH <richard.h.beach@cgi.com>
2019-01-23 16:52:15 -0500

    Merge branch 'development' of https://alm.cgifederal.com/scm/scm/epacdx/e-enterprise-2.0 into EE-2951-use-axios-request-to-load-login-data
    # Conflicts:
    #	app/src/components/MainHeader.vue
    #	app/src/store/mutations.js
    #	app/src/store/state.js
    #	app/src/store/types.js
    #	app/src/views/Login.vue

--- app/src/store/state.js
+++ app/src/store/state.js
@@ -10,19 +10,19 @@ const state = {
         LOCAL: {
             relyingParty: 'https://apidev2.e-enterprise.gov/',
             issuer: 'https://extauthdev.epacdxnode.net',
-            sendBridgeBackTo: 'https://apidev2.e-enterprise.gov/user/authenticate',
+            sendBridgeBackTo: 'https://apidev2.e-enterprise.gov/authenticate/user',
             signInMethod: 'wsignin1.0',
         },
         DEV: {
             relyingParty: 'https://apidev2.e-enterprise.gov/',
             issuer: 'https://extauthdev.epacdxnode.net',
-            sendBridgeBackTo: 'https://apidev2.e-enterprise.gov/user/authenticate',
+            sendBridgeBackTo: 'https://apidev2.e-enterprise.gov/authenticate/user',
             signInMethod: 'wsignin1.0',
         },
         TEST: {
             relyingParty: 'https://apitest2.e-enterprise.gov/',
             issuer: 'https://extauthtest.epacdxnode.net',
-            sendBridgeBackTo: 'https://apitest2.e-enterprise.gov/user/authenticate',
+            sendBridgeBackTo: 'https://apitest2.e-enterprise.gov/authenticate/user',
             signInMethod: 'wsignin1.0',
         },
     },
