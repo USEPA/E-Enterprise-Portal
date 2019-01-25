bbdb501f145fffddc7db5b071938477ddb639972
RBEACH <richard.h.beach@cgi.com>
2019-01-25 12:58:31 -0500

    changed web config to match dev

--- app/src/components/MainHeader.vue
+++ app/src/components/MainHeader.vue
@@ -29,14 +29,16 @@
                 id="try-it"
                 class="btn btn-sm"
                 variant="primary"
+                @mouseover="addTryItArrow"
+                @mouseleave="removeTryItArrow"
                 :title="tryitTitle"
                 v-if='!isLoggedIn'>
-                <i class="fas fa-arrow-circle-right fa-arrow-alt-from-left"></i>&nbsp;Try It
+                <i class="fas fa-arrow-circle-right fa-arrow-alt-from-left pr-1"/>Try It
               </b-button>
             </router-link>
           </template>
         </div>
-        <div class="col-4-md">
+        <div class="col-4-md" id="log-in-container">
           <template v-if='isLoggedIn'>
             <span>Welcome {{ username }}</span>
             <b-btn
@@ -44,35 +46,56 @@
               variant="outline-secondary"
               class="btn btn-sm btn-outline-primary account-auth"
               @click="userLogOut">
-              <i class="fas fa-lock"></i>&nbsp;
+              <i class="fas fa-lock pr-1"/>
               Logout
             </b-btn>
-           <b-button
-                to="/User"
-                id="my-account"
-                class="btn btn-sm ml-2"
-                variant="primary">
-                My account
-           </b-button>
-        </template>
+            <b-button
+                 to="/User"
+                 id="my-account"
+                 class="btn btn-sm ml-2"
+                 variant="primary">
+                 My account
+            </b-button>
+          </template>
           <template v-else>
-            <div class="router-link-wrapper pt-2">
-                <a href="javascript:void(0);" @click="handleLogin" class="btn btn-sm btn-outline-primary account-auth-login">
-                    <i class="fas fa-lock"></i>&nbsp;
+              <div
+                id="login-arrow"
+                ref="login-arrow"
+                class="d-none"
+              />
+              <div class="router-link-wrapper pt-2" id="log-in">
+                  <a
+                      href="javascript:void(0);"
+                      @mouseover="addLogInArrow"
+                      @mouseleave="removeLogInArrow"
+                      @click="handleLogin"
+                      class="btn btn-sm btn-outline-primary account-auth-login">
+                    <i class="fas fa-lock pr-1"/>
                     Login
-                    <span class="arrow-down-message small mt-3">{{ loginBtnHoverMessage }}</span>
-                </a>
+                  </a>
             </div>
           </template>
         </div>
+          <b-tooltip
+                  target="try-it"
+                  class="tryit-tooltip"
+                  placement="bottom">
+              Want to just try it? No log in needed.
+          </b-tooltip>
+          <b-tooltip
+                  target="log-in"
+                  class="login-tooltip"
+                  placement="bottom">
+              Use an EPA, CDX, or a social media account to login
+          </b-tooltip>
       </div>
     </div>
   </header>
 </template>
 
 <script>
-  import { mapGetters, mapActions } from 'vuex';
-  import { EventBus } from '../EventBus';
+    import { mapGetters, mapActions } from 'vuex';
+    import { EventBus } from '../EventBus';
 
   // eslint-disable-next-line
   export default {
@@ -94,6 +117,22 @@
         'userLogOut',
         'handleLogin',
       ]),
+      removeTryItArrow() {
+          this.$refs['tryit-arrow'].classList.remove('d-block');
+          this.$refs['tryit-arrow'].classList.add('d-none');
+      },
+      addTryItArrow() {
+          this.$refs['tryit-arrow'].classList.remove('d-none');
+          this.$refs['tryit-arrow'].classList.add('d-block');
+      },
+      removeLogInArrow() {
+          this.$refs['login-arrow'].classList.remove('d-block');
+          this.$refs['login-arrow'].classList.add('d-none');
+      },
+      addLogInArrow() {
+          this.$refs['login-arrow'].classList.remove('d-none');
+          this.$refs['login-arrow'].classList.add('d-block');
+      },
       dumyLogOut() {
         this.userLogOut();
       },
@@ -108,6 +147,45 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped
   lang="scss">
+    #try-it-container {
+        position: relative;
+    }
+    #try-it {
+        position: relative;
+    }
+    #log-in-container {
+        position: relative;
+    }
+    #log-in {
+        position: relative;
+    }
+    #tryit-arrow {
+        top: 100%;
+        left: 50%;
+        border: .7rem solid transparent;
+        content: " ";
+        height: 0;
+        width: 0;
+        position: absolute;
+        pointer-events: none;
+        border-color: rgba(0, 98, 160, 0);
+        border-top-color: #0062a0;
+        margin-left: -.7rem;
+    }
+    #login-arrow {
+        top: 100%;
+        left: 50%;
+        border: .7rem solid transparent;
+        content: " ";
+        height: 0;
+        width: 0;
+        position: absolute;
+        pointer-events: none;
+        border-color: rgba(0, 98, 160, 0);
+        border-top-color: #007ac6;
+        margin-left: -.7rem;
+    }
+
   .eep_logo {
     img {
       max-width: 100%;
