fcf3b38b188dd562ac348dae5d8d24731ac46bdc
mculpepp <michael.culpepper@cgifederal.com>
2019-01-29 14:37:36 -0600

    scoped scss from EE-2929 & dynamic id for basic page content

--- app/src/components/BasicPage.vue
+++ app/src/components/BasicPage.vue
@@ -1,11 +1,10 @@
 <template>
-  <div id="basic-page-container">
-    <h1 v-if="basicPages[0]">{{page.title[0].value}}</h1>
+  <div :id="urlAlias">
+    <h1 v-if="basicPages[0]">{{ page.title[0].value }}</h1>
     <div
       v-if="basicPages[0]"
       v-html="page.body[0].value"
-      id="basic-page">
-    </div>
+      id="basic-page-content"/>
     <AppPlaceholderContent
       v-if="!basicPages[0]"
       :repeatitions="3">
@@ -13,13 +12,13 @@
         v-for="i in 3"
         :key="i"
         class="row my-5">
-        <h5 class="col-md-12 pulse"></h5>
+        <h5 class="col-md-12 pulse"/>
         <div
           v-for="j in 3"
           :key="`${j}-left`"
           class="col-md-6">
           <div class="row m-2">
-            <div class="col-12 pulse"></div>
+            <div class="col-12 pulse"/>
           </div>
         </div>
         <div
@@ -27,7 +26,7 @@
           :key="`${j}-right`"
           class="col-md-6">
           <div class="row m-2">
-            <div class="col-12 pulse"></div>
+            <div class="col-12 pulse"/>
           </div>
         </div>
       </div>
@@ -42,7 +41,7 @@
   import { AppPlaceholderContent } from '../modules/wadk/WADK';
 
   export default {
-    name: "basic-page",
+    name: 'BasicPage',
     components: {
       AppPlaceholderContent,
     },
@@ -56,16 +55,56 @@
       ...mapGetters({
         basicPages: 'getBasicPagesArray',
       }),
-      page () {
-        return this.basicPages.find(page => page.path[0].alias.replace(/\\|\//g,'') === this.urlAlias);
+      page() {
+        return this.basicPages.find(page => page.path[0].alias.replace(/\\|\//g, '') === this.urlAlias);
       },
     },
     mounted() {
-      console.log(this.basicPages.find(page => page.path[0].alias.replace(/\\|\//g,'') === this.urlAlias));
-    }
+      console.log(this.basicPages.find(page => page.path[0].alias.replace(/\\|\//g, '') === this.urlAlias));
+    },
   };
 </script>
 
-<style scoped>
+<style scoped
+  lang="scss">
+  #for-partners {
+  }
+  h2 + p,
+  h2 + table,
+  p + pre
 
+  { margin-top: 5px; }
+  pre + h2
+
+  { margin-top: 2em; }
+  pre
+
+  { margin-left: 0;
+    overflow: auto;
+    max-height: 200px;
+    padding: 20px;
+    background-color: #fcf6db;
+    border: 1px solid #e5e0c6;
+    font-size: 0.85em;
+    white-space: pre-wrap;
+    white-space: -moz-pre-wrap;
+    white-space: -o-pre-wrap;
+    word-wrap: break-word; }
+  table {
+    td
+
+    { vertical-align: top; }
+    ol {
+      margin-top: 0;
+      margin-bottom: 0;
+      li {
+        margin-top: 0;
+        margin-bottom: 0.5rem;
+        background-color: transparent;
+        &:before
+
+        { font-weight: normal; }
+      }
+    }
+  }
 </style>
