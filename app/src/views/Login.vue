2de4a8869561e3cb9ed182ac1107530db8f99986
RBEACH <richard.h.beach@cgi.com>
2019-01-29 15:23:04 -0500

    changed web config to match dev

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -104,6 +104,9 @@
                 'navigateToBridge',
             ]),
         },
+        beforeCreate(){
+            this.$store.dispatch('handleLogin');
+        },
         computed: {
             ...mapGetters({
                 bridgeURL: 'getBridgeURL',
