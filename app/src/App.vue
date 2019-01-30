01f981babd35271c10281ebe90af77842e6beebe
RBEACH <richard.h.beach@cgi.com>
2019-01-30 18:18:46 -0500

    Merge branch 'development' of https://alm.cgifederal.com/scm/scm/epacdx/e-enterprise-2.0 into EE-2975-jwt-token-from-api-is-redirect-to
    # Conflicts:
    #	app/src/App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -140,25 +140,47 @@
   });
   },
   beforeMount(){
+    // Declare the main url that the page is currently on
+    const main_url = window.location.href;
     // Declare the store
     const vm = this;
     const store = vm.$store;
-
-    console.log(document.cookie);
-
-//    if(!this.$cookie.get('userLoggedIn')){
-//        // Log user in and set user name
-//        store.commit('USER_LOG_IN');
-//        store.commit(types.SET_UID, this.$cookie.get('uid'));
-//
-//        AppAxios.get(`${this.$store.getters.getEnvironmentApiURL}/user/${this.$cookie.get('uid')}?_format=json`, {
-//            headers: { Authorization: `Bearer ${this.$cookie.get('token')}` },
-//        }).then((response) => {
-//            store.commit('SET_USER_OBJECT', response.data);
-//        }).catch((error) => {
-//            console.warn(error)
-//        });
-//    }
+    if (main_url.indexOf("token") > -1 && main_url.indexOf("uid") > -1) {
+      // Declare variables
+      let vars = {};
+      // Extracts the URL params
+      // Got this functionality from https://html-online.com/articles/get-url-parameters-javascript/
+      let parts = main_url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        vars[key] = value;
+      });
+      // find the URL params for each one
+      const token = vars["token"];
+      const uid = vars["uid"];
+      // Have to do it this way for cross browser method: https://scotch.io/tutorials/how-to-encode-and-decode-strings-with-base64-in-javascript
+      // Set another cookie saying they logged in
+      this.$cookie.set('userLoggedIn', true, {expires: '20m'});
+      // set user token in cookie
+      this.$cookie.set('Token', token, {expires: '20m'});
+      this.$cookie.set('uid', uid, {expires: '20m'});
+      store.commit(types.SET_UID, uid);
+      // Log user in
+      store.commit('USER_LOG_IN');
+    }else{
+      if(this.$cookie.get('userLoggedIn')){
+        // Log user in and set user name
+        store.commit('USER_LOG_IN');
+        store.commit(types.SET_UID, this.$cookie.get('uid'));
+      }
+    }
+    if (this.$cookie.get('userLoggedIn')) {
+      AppAxios.get(`${this.$store.getters.getEnvironmentApiURL}/user/${this.$cookie.get('uid')}?_format=json`, {
+          headers: { Authorization: `Bearer ${this.$cookie.get('Token')}` },
+      }).then((response) => {
+          store.commit('SET_USER_OBJECT', response.data);
+      }).catch((error) => {
+          console.warn(error)
+      });
+    }
   },
   mounted() {
     //  [App.vue specific] When App.vue is finish loading finish the progress bar
