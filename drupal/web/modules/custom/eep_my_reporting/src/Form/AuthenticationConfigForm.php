55567a6ab466b732727bef301484fcd832740326
Matkin, Benjamin K (CGI Federal) <benjamin.matkin@cgifederal.com>
2019-01-31 14:49:27 -0600

    Adding config for my reporting

---
+++ drupal/web/modules/custom/eep_my_reporting/src/Form/AuthenticationConfigForm.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @file
+ *
+ * Contains Drupal\eep_bridge\Form\AuthenticationConfigForm
+ */
+namespace Drupal\eep_my\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class AuthenticationConfigForm extends ConfigFormBase{
+
+
+  /**
+   * @return array
+   */
+  protected function getEditableConfigNames() {
+    return ['eep_bridge.environment_settings'];
+  }
+
+  /**
+   * @return string
+   */
+  public function getFormId() {
+    return 'eep_bridge_environment_form';
+  }
+
+  /**
+   * @param array $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *
+   * @return array
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('eep_bridge.environment_settings');
+
+    $form['eep_bridge_environment_name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Current Environment Host Name'),
+      '#description' => $this->t('E.g: https://dev2.e-enterprise.gov'),
+      '#default_value' => $config->get('eep_bridge_environment_name'),
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+    $this->config('eep_bridge.environment_settings')
+      ->set('eep_bridge_environment_name', $form_state->getValue('eep_bridge_environment_name'))
+      ->save();
+  }
+
+
+}
