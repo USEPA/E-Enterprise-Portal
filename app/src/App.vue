d4743b571100fe2a52ace3329255f153ce3db701
RBEACH <richard.h.beach@cgi.com>
2019-02-05 11:21:40 -0500

    removed config install

--- app/src/App.vue
+++ app/src/App.vue
@@ -223,7 +223,7 @@
 
               // Function that is used everytime setInterval is called
               function checkCookieExistance(){
-                  console.log('token: ' + store.getters.getLoggedInToken + '     how many minutes left: ' + Date.now().getMinutes() - store.getters.getLogInTime.getMinutes())
+                  console.log('token: ' + store.getters.getLoggedInToken + '     how many minutes left: ' + (new Date).getMinutes() - store.getters.getLogInTime.getMinutes())
                   // If statement will only execute when there is one minute left until expiration
                   if(document.cookie.indexOf('Token=') === -1 && store.getters.getDisplayLoggedInElements){
                       console.log("hit check cookie existance inside if");
