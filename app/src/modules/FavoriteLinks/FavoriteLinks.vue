eb5583389aa2c263607a3dfcc7d4aa354fdce794
mculpepp <michael.culpepper@cgifederal.com>
2019-02-06 16:13:29 -0600

    FAVLINKS fix

--- app/src/modules/FavoriteLinks/FavoriteLinks.vue
+++ app/src/modules/FavoriteLinks/FavoriteLinks.vue
@@ -129,14 +129,13 @@
           <b-form-input
             type="text"
             v-model="editModalInfo.first"
-            required>
-          </b-form-input>
+            required/>
           <label>Website Address (URL)
           </label>
           <b-form-input
             type="url"
             v-model="editModalInfo.second"
-            required></b-form-input>
+            required/>
           <b-btn
             class="mt-3"
             variant="primary"
