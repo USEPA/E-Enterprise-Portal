bf263c430a7a4477bd8c1c18664468b0b9f33a40
hkunatha <himabindu.kunatha@cgifederal.com>
2019-02-14 13:34:56 -0600

    Default Changelist

--- app/src/views/User.vue
+++ app/src/views/User.vue
@@ -72,7 +72,7 @@
                   <div>
                     <h6>Organization</h6>
                     <b-form-select v-model="selected" id="org-selection" class="mr-3" required>
-                      <option v-if= "organisation.length===0" v-bind:value="null">-None-</option>
+                      <option v-if="organisation.length===0"v-bind:value="null">-None-</option>
                       <option v-else v-bind:value="null">{{organisation[0].second}}</option>
                       <option>-None-</option>
                       <option v-for="option in org" v-bind:value="option.name">{{ option.name }}
@@ -83,7 +83,7 @@
                   <div>
                     <h6>Role</h6>
                     <b-form-select v-model="selectedRole" id="role-selection" class="mr-3" required>
-                      <option v-if= "role.length===0" v-bind:value="null">-None-</option>
+                      <option v-if="role.length===0" v-bind:value="null">-None-</option>
                       <option v-else v-bind:value="null">{{role[0].second}}</option>
                       <option>-None-</option>
                       <option
