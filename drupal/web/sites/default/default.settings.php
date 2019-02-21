72cb1c90f321b7f3cf11b8eb50f62a9910313b56
RBEACH <richard.h.beach@cgi.com>
2019-02-21 09:43:08 -0500

    Merge branch 'development' of https://alm.cgifederal.com/scm/scm/epacdx/e-enterprise-2.0 into feature/EE-3162-eep-2.0-user-profile-location-services
    # Conflicts:
    #	app/src/store/actions.js

--- drupal/web/sites/default/default.settings.php
+++ drupal/web/sites/default/default.settings.php
@@ -768,6 +768,3 @@ $settings['entity_update_batch_size'] = 50;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-$config_directories['sync'] = '../config/sync';
-$settings['install_profile'] = 'e_enterprise_portal';
-$config['system.passwords']['cdx_naas'] = '__PASSWORD__';
