2787a6687de041e2abbf192b204c0cb4866afa0c
RBEACH <richard.h.beach@cgi.com>
2019-02-22 14:02:49 -0500

    removed token from key.key

--- drupal/web/index.php
+++ drupal/web/index.php
@@ -7,6 +7,8 @@
  * All Drupal code is released under the GNU General Public License.
  * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
  */
+
+
 use Drupal\Core\DrupalKernel;
 use Symfony\Component\HttpFoundation\Request;
 
