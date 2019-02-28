571ef24ee3d58504e4e7681ff3b3bbad8c9555b8
mculpepp <michael.culpepper@cgifederal.com>
2019-02-28 12:46:52 -0600

    basic framework for MyCertifications

--- drupal/web/profiles/e_enterprise_portal/import-content.php
+++ drupal/web/profiles/e_enterprise_portal/import-content.php
@@ -13,6 +13,7 @@ require_once DRUPAL_WEB_DIR . '/core/modules/search/search.module';
 require_once DRUPAL_WEB_DIR . '/core/modules/node/node.module';
 
 $default_content_paths = [
+  'modules/custom/eep_my_certifications/content/my-certifications.json',
   'modules/custom/eep_proxy_service/content/be-well-informed-service.json',
   'modules/custom/eep_proxy_service/content/location-service.json',
   'modules/custom/eep_proxy_service/content/geolocation.json',
