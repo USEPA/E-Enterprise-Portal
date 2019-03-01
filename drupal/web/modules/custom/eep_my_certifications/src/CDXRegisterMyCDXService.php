52342ebc5e3ccd9eefb1e5983aa8104e51d49445
mculpepp <michael.culpepper@cgifederal.com>
2019-03-01 08:40:15 -0600

    EEP MyCerts find + replace version of MyReporting

---
+++ drupal/web/modules/custom/eep_my_certifications/src/CDXRegisterMyCDXService.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\eep_my_certifications;
+
+use Drupal\eep_my_certifications\CDXNaasService;
+use Drupal\eep_my_certifications\SOAPHandler;
+
+
+class CDXRegisterMyCdxService extends CDXNaasService {
+
+  private $soap_handler;
+
+  private $config;
+
+  private $system_config;
+
+  function __construct($config) {
+    $this->config = $config;
+    $this->system_config = \Drupal::config('system.passwords');
+    $this->soap_handler = new SOAPHandler();
+    $this->wsdl = $this->config->get('wsdl');
+    $soap_service_setup = $this->soap_handler->connectToSOAPServerWithWSDL($this->wsdl);
+    if ($soap_service_setup->error) {
+      $this->client = FALSE;
+    }
+    else {
+      $this->token_access_name = 'securityToken';
+      $this->token_generation_method = 'Authenticate';
+      $this->client = $soap_service_setup->client;
+      $this->generate_token_params();
+      $this->generate_token();
+    }
+  }
+
+  private function generate_token_params() {
+    $this->params = [
+      "userId" => $this->config->get('admin_id'),
+      "credential" => $this->system_config->get('cdx_naas'),
+      "domain" => $this->config->get('domain'),
+      "authenticationMethod" => $this->config->get('authentication_method'),
+    ];
+  }
+
+  private function return_handoff_params($role_id) {
+    $params = [
+      'securityToken' => $this->token,
+      'userRoleId' => $role_id,
+      'clientIp' => $_SERVER['SERVER_ADDR'],
+    ];
+    return $params;
+  }
+
+  public function return_link_handoff_data($role_id) {
+    $this->params = $this->return_handoff_params($role_id);
+    return $this->call_service_method('RetrieveMyCdxLinkHandoff');
+  }
+
+  public function retrieve_cdx_links_for_cdx_username($cdx_username) {
+    $this->params = [$this->token_access_name => $this->token];
+    $this->params['userId'] = $cdx_username;
+    return $this->call_service_method('RetrieveMyCdxLinks');
+
+  }
+}
---
+++ drupal/web/modules/custom/eep_my_certifications/src/Form/AuthenticationConfigForm.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * @file
+ *
+ * Contains Drupal\eep_eep_my_certifications\Form\AuthenticationConfigForm
+ */
+
+namespace Drupal\eep_my_certifications\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class AuthenticationConfigForm extends ConfigFormBase {
+
+
+  /**
+   * @return array
+   */
+  protected function getEditableConfigNames() {
+    return ['eep_my_certifications.form'];
+  }
+
+  /**
+   * @return string
+   */
+  public function getFormId() {
+    return 'eep_my_certifications_form';
+  }
+
+  /**
+   * @param array $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *
+   * @return array
+   *
+   *
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('eep_my_certifications.form');
+    $form['wsdl'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('WSDL address to make SOAP calls to'),
+      '#description' => $this->t('A URL ending with \'?wsdl\''),
+      '#default_value' => $config->get('wsdl'),
+    ];
+    $form['security_token_wsdl'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('WSDL address to generate SSO Security token'),
+      '#description' => $this->t('A URL ending with \'?wsdl\''),
+      '#default_value' => $config->get('security_token_wsdl'),
+    ];
+    $form['cdx_base_url'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('CDX URL for SSO and silent handoffs'),
+      '#description' => $this->t('This URL is used for the SSO redirect to CDX'),
+      '#default_value' => $config->get('cdx_base_url'),
+    ];
+    $form['cdx_submission_history_url'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Submission History Return URL for SSO'),
+      '#description' => $this->t('This URL is used for the SSO redirect to CDX'),
+      '#default_value' => $config->get('cdx_submission_history_url'),
+    ];
+    $form['admin_id'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Administrative ID for NAAS authentication'),
+      '#description' => $this->t('Typically an email address'),
+      '#default_value' => $config->get('admin_id'),
+    ];
+    $form['domain'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Domain for NAAS authentication'),
+      '#description' => $this->t('Typically \'default\''),
+      '#default_value' => $config->get('domain'),
+    ];
+    $form['authentication_method'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Authentication method for NAAS authentication'),
+      '#description' => $this->t('Typically \'password\''),
+      '#default_value' => $config->get('authentication_method'),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+    $this->config('eep_my_certifications.form')
+      ->set('wsdl', $form_state->getValue('wsdl'))
+      ->set('security_token_wsdl', $form_state->getValue('security_token_wsdl'))
+      ->set('cdx_base_url', $form_state->getValue('cdx_base_url'))
+      ->set('cdx_submission_history_url', $form_state->getValue('cdx_submission_history_url'))
+      ->set('admin_id', $form_state->getValue('admin_id'))
+      ->set('domain', $form_state->getValue('domain'))
+      ->set('authentication_method', $form_state->getValue('authentication_method'))
+      ->save();
+  }
+
+
+}
