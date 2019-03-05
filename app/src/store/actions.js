136b9c97d0ebc7bea2656ce738e522470cdc5d51
RBEACH <richard.h.beach@cgi.com>
2019-03-05 15:46:06 -0500

    Merge branch 'development' of https://alm.cgifederal.com/scm/scm/epacdx/e-enterprise-2.0 into bugfix/EE-3043-logout-button-does-not-log-user-out
    # Conflicts:
    #	app/src/store/actions.js

--- app/src/store/actions.js
+++ app/src/store/actions.js
@@ -110,25 +110,25 @@ export default {
   userLogOut(context) {
     const store = context;
 
-    AppAxios.get(`${store.getters.getEnvironmentApiURL}/authenticate/dev_user/logout`, {
+    // add additional logout logic here
+    Vue.cookie.set('Token', false, { expires: '-99s' });
+    Vue.cookie.set('uid', false, { expires: '-99s' });
+    Vue.cookie.set('userLoggedIn', false, { expires: '-99s' });
+    Vue.cookie.set('userLogInTime', '', { expires: '-99s' });
+    store.commit('IS_USER_LOGGED_IN', false);
+      // Reset login token and time
+      store.commit(types.SET_LOGGED_IN_TOKEN, '');
+      store.commit(types.SET_LOGGED_IN_TIME, '');
+
+
+      AppAxios.get(`${store.getters.getEnvironmentApiURL}/authenticate/dev_user/logout`, {
         headers: store.GETHeaders
     }).then((response) => {
+        console.log(response);
 
-       // Reset cookies
-       Vue.cookie.set('Token', false, { expires: '-99s' });
-       Vue.cookie.set('uid', false, { expires: '-99s' });
-       Vue.cookie.set('userLoggedIn', false, { expires: '-99s' });
-       Vue.cookie.set('userLogInTime', '', { expires: '-99s' });
 
-       store.commit('IS_USER_LOGGED_IN', false);
-
-       // Reset login token and time
-       store.commit(types.SET_LOGGED_IN_TOKEN, '');
-       store.commit(types.SET_LOGGED_IN_TIME, '');
-
-       // Redirect window to the bridge
-       window.location.href = response.data.logout_url;
-
+        // Redirect window to the bridge
+        window.location.href = response.logout_url;
     }).catch((error) => {
         console.warn(error.response);
     });
@@ -358,13 +358,17 @@ export default {
         store.commit(types.SET_UID, Vue.cookie.get('uid'));
 
         /**
-          * Load EEPUser info and do not push to workbench if already logged in
-          * unless loading homepage. This fixes footer links so they dont auto redirect
-          * to workbench.
+         * Load EEPUser info and do not push to workbench if already logged in
+         * unless loading homepage. This fixes footer links so they dont auto redirect
+         * to workbench.
          */
-       user
+        if (router.history.current.path === '/') {
+            store.dispatch('loadEEPUser').then(router.push('/workbench'));
+        } else {
+            store.dispatch('loadEEPUser');
+        }
       }
-
+      
       store.dispatch('checkCookie', payload);
       //  [App.vue specific] When App.vue is finish loading finish the progress bar
       vm.$Progress.finish();
