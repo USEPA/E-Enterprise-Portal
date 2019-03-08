7dfa3d5ff8c248d15fba5285e1f46cf0bf93caf5
mculpepp <michael.culpepper@cgifederal.com>
2019-03-08 09:28:33 -0600

    permitType dropdown

--- app/src/modules/PermitSearch/PermitSearch.vue
+++ app/src/modules/PermitSearch/PermitSearch.vue
@@ -3,60 +3,81 @@
     <AppWrapper
       :eep-app="eepApp"/>
     <!-- base form -->
-    <b-form
-      class="needs-validation"
-      @submit="initialFormSubmit"
-      novalidated>
-      <label for="permit-type-selection">Select permit type</label>
-      <b-form-select
-        id="permit-type-selection"
-        :value="permitType"
-        :options="formOptions.permitType"
-        ref="permitTypeDropdown"
-        @change="setPermitType"
-        class="mb-3"
-        size="sm"
-        required>
-        <template slot="first">
-          <option>{{ permitType }}</option>
-        </template>
-      </b-form-select>
-      <br>
-      <label for="facility-name-input">Facility Name</label>
-      <b-form-input
-        id="facility-name-input"
-        ref="facility-name-input"
-        type="string"
-        size="sm"
-        @change="setFacilityName"/>
-      <b-btn
-        variant="primary"
-        ref="btnCheckYourWater"
-        type="submit">
-        Search
-      </b-btn>
+    <div id="permit-search-wapp-inner">
+      <div v-if="eepApp.size === 'small'">
+        <b-form
+          class="needs-validation"
+          @submit="initialFormSubmit"
+          novalidated>
+          <label
+            class="mb-0"
+            for="permit-type-selection">Select permit type</label>
+          <b-form-select
+            id="permit-type-selection"
+            :value="permitType"
+            :options="formOptions.permitType"
+            ref="permitTypeDropdown"
+            @change="setPermitType"
+            size="sm"
+            required>
+            <template slot="first">
+              <option>{{ permitType }}</option>
+            </template>
+          </b-form-select>
+          <br>
+          <label
+            class="mb-0"
+            for="facility-name-input">Facility Name</label>
+          <b-row>
+            <b-col md="8">
+              <b-form-input
+                id="facility-name-input"
+                ref="facility-name-input"
+                type="string"
+                size="sm"
+                class="mb-2"
+                @change="setFacilityName"/>
+            </b-col>
+            <b-col md="4">
+              <b-btn
+                size="sm"
+                variant="primary"
+                ref="btnCheckYourWater"
+                type="submit">
+                Search
+              </b-btn>
+            </b-col>
+          </b-row>
+        </b-form>
+        <b-row>
+          <b-col class="permit-search-footer">
+            <a>Advanced Search</a> &#8226; <a>What permits can I find?</a>
+          </b-col>
+        </b-row>
 
-    </b-form>
+        <!-- Main Permit Search Modal-->
+        <AppModal
+          id="permit-search-modal"
+          modal-ref="permit-search-modal"
+          title="Permit Search tool"
+          :hide-footer="true">
+          {{ permitType }}
+          <br>
+          {{ facilityName }}
 
-    <!-- Main Permit Search Modal-->
-    <AppModal
-      id="permit-search-modal"
-      modal-ref="permit-search-modal"
-      title="Permit Search tool"
-      :hide-footer="true">
-      {{ permitType }}
-        <br>
-      {{ facilityName }}
-
-    </AppModal>
-
+        </AppModal>
+      </div>
+      <div v-else-if="eepApp.size === 'large'">
+        large
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
   import { mapActions, mapGetters } from 'vuex';
-  import { AppWrapper, AppModal, AppPlaceholderContent } from '../wadk/WADK';
+  import { AppWrapper, AppModal } from '../wadk/WADK';
   import storeModule from './store/index';
 
   const moduleName = 'PermitSearch';
@@ -80,7 +101,7 @@
       return {};
     },
     mounted() {
-
+      console.log(this.eepApp.size);
     },
     computed: {
       ...mapGetters(moduleName, {
