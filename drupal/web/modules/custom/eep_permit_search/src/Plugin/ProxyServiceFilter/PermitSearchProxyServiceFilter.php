be6b45c5c1577c5c6157405ac3165a3a14a3d4f1
mculpepp <michael.culpepper@cgifederal.com>
2019-03-09 14:19:09 -0600

    getting and manipulating responses

--- drupal/web/modules/custom/eep_permit_search/src/Plugin/ProxyServiceFilter/PermitSearchProxyServiceFilter.php
+++ drupal/web/modules/custom/eep_permit_search/src/Plugin/ProxyServiceFilter/PermitSearchProxyServiceFilter.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\eep_permit_search\Plugin\ProxyServiceFilter;
 
-use Drupal\eep_core\Helpers\ArrayHelper;
 use Drupal\eep_proxy_service\Plugin\ProxyServiceFilterBase;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Stream;
@@ -13,14 +12,13 @@ use GuzzleHttp\Psr7\Response;
  * Class PermitSearchProxyServiceFilter
  *
  * @ProxyServiceFilter(
- *   id = "permit-search",
+ *   id = "permitsearch",
  *   label = @Translation("Permit Search Filter")
  * )
  *
  * @package Drupal\eep_core\Plugin\ProxyServiceFilter
  */
-class PermitSearchProxyServiceFilter extends ProxyServiceFilterBase
-{
+class PermitSearchProxyServiceFilter extends ProxyServiceFilterBase {
 
     /**
      * MUST override this function to prevent the default behavior
@@ -37,7 +35,40 @@ class PermitSearchProxyServiceFilter extends ProxyServiceFilterBase
      */
     public function fetch()
     {
+        // Get the query values
+        $query = $this->getIncomingRequest()->query->all();
 
+        // Add the important query information into the payload for processing later
+        (!isset($query['all_forms'])) ?: $this->payload['all_forms'][] = trim($query['all_forms']);
+        (!isset($query['form_types'])) ?: $this->payload['form_types'][] = trim($query['form_types']);
+        (!isset($query['form_status'])) ?: $this->payload['form_status'][] = trim($query['form_status']);
+
+
+        // get all forms
+        if(isset($query['all_forms'])) {
+            $form_url = $this->request->getUri() . 'form';
+            $form_response = $this->make_request_and_receive_response($form_url);
+            $response = json_decode($form_response->getBody(), FALSE);
+            $this->payload['all_forms'] = $response;
+        }
+
+        // get form_types
+        if(isset($query['form_types'])) {
+            $form_url = $this->request->getUri() . 'reference/formType';
+            $form_response = $this->make_request_and_receive_response($form_url);
+            $response = json_decode($form_response->getBody(), FALSE);
+            $this->payload['form_types'] = $response;
+        }
+
+        // get form_status
+        if(isset($query['form_status'])) {
+            $form_url = $this->request->getUri() . 'reference/formStatus';
+            $form_response = $this->make_request_and_receive_response($form_url);
+            $response = json_decode($form_response->getBody(), FALSE);
+            $this->payload['form_status'] = $response;
+        }
+
+        return $response;
     }
 
     /**
@@ -47,6 +78,27 @@ class PermitSearchProxyServiceFilter extends ProxyServiceFilterBase
      */
     public function postfetch()
     {
+        // Build content
+        $content = [];
+
+        if($this->payload['all_forms'] != null) {
+            $content['all_forms'] = $this->payload['all_forms'];
+        }
+        if ($this->payload['form_types'] != null) {
+            $content['form_types'] = $this->payload['form_types'];
+        }
+        if ($this->payload['form_status'] != null) {
+            $content['form_status'] = $this->payload['form_status'];
+        }
+
+        $final_content = \GuzzleHttp\json_encode($content);
+
+        // Update the response
+        $this->response = new Response(
+            200,
+            ['Content-Type' => ['application/json']],
+            $final_content
+        );
         return $this->response;
     }
 
@@ -64,7 +116,7 @@ class PermitSearchProxyServiceFilter extends ProxyServiceFilterBase
             'GET',
             $request_url,
             [
-                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
             ]
         );
         $guzzle_client = new Client();
