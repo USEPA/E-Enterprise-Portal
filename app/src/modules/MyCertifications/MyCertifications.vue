faa6d3be97afc714c8ecf002fa300a7ff7c9b269
jrockwei <john.rockweiler@cgifederal.com>
2019-03-15 07:20:37 -0500

    BWI PDF

--- app/src/modules/MyCertifications/MyCertifications.vue
+++ app/src/modules/MyCertifications/MyCertifications.vue
@@ -101,7 +101,12 @@
           :total-rows="totalRows"
           :per-page="datatableSettings.perPage"
           v-model="datatableSettings.currentPage"
-          class="my-0"/>
+          class="my-0">
+          <div class="wapp-arrows" slot="first-text"><img src="/images/pager-first.png"></div>
+          <div class="wapp-arrows" slot="next-text"><img src="/images/pager-next.png"></div>
+          <div class="wapp-arrows" slot="prev-text"><img src="/images/pager-previous.png"></div>
+          <div class="wapp-arrows" slot="last-text"><img src="/images/pager-last.png"></div>
+        </b-pagination>
       </b-col>
     </b-row>
 
