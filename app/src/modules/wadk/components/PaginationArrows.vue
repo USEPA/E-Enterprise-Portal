16ff8475828449e450e3328965dab1024fcc33dd
jrockwei <john.rockweiler@cgifederal.com>
2019-03-15 09:44:04 -0500

    Components added

--- app/src/modules/wadk/components/PaginationArrows.vue
+++ app/src/modules/wadk/components/PaginationArrows.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="wapp-arrows" slot="first-text"><img src="/images/pager-first.png"></div>
-  <div class="wapp-arrows" slot="next-text"><img src="/images/pager-next.png"></div>
-  <div class="wapp-arrows" slot="prev-text"><img src="/images/pager-previous.png"></div>
-  <div class="wapp-arrows" slot="last-text"><img src="/images/pager-last.png"></div>
-</template>
+  <div>
+    <div class="wapp-arrows"
+      slot="first-text"><img src="/images/pager-first.png"></div>
 
+    <div class="wapp-arrows"
+      slot="next-text"><img src="/images/pager-next.png"></div>
+
+    <div class="wapp-arrows"
+      slot="prev-text"><img src="/images/pager-previous.png"></div>
+
+    <div class="wapp-arrows"
+      slot="last-text"><img src="/images/pager-last.png"></div>
+  </div>
+</template>
 <script>
   export default {
     name: "PaginationArrows"
