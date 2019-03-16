5a5dd0a4040f650210a2e31fcfef4f282c2f40ab
mculpepp <michael.culpepper@cgifederal.com>
2019-03-16 13:19:10 -0500

    PERMITS

--- app/src/components/MainFooter.vue
+++ app/src/components/MainFooter.vue
@@ -1,30 +1,6 @@
 <template>
   <footer class="footer">
     <div
-      class="alert alert-info"
-      role="alert"
-      v-if="['workbench'].indexOf($route.name) > -1 &&
-      authenticated === false && (termsAndConditionsCookie === false || termsAndConditionsCookie == undefined)">
-      <div
-        id="terms-conditions"
-        class="container w-100 d-flex text-dark">
-        <div class="row align-self-center w-100">
-          <div class="col-12 text-center">
-            <span>By using the E-Enterprise Portal, you agree to the </span>
-            <a
-              href="https://cdx.epa.gov/Terms"
-              target="_blank"
-              class="text-dark font-weight-bold">
-              Terms and Conditions</a>
-            <span
-              class="text-decoration-underline cursor-pointer font-weight-bold"
-              @click="onTermsAndConditions"
-              data-dismiss="alert">Continue »</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div
       v-if="footerLinksLoaded && footerLinks.length > 0"
       class="container py-2">
       <div class="row justify-content-center small">
@@ -64,53 +40,31 @@
       <div class="row justify-content-center small">
         <div
           class="col-auto">
-          Failed to load Footer...
+          Failed to load Footer.
         </div>
       </div>
       <div class="row justify-content-center small">
         <div class="col-auto text-align-center small">
-          Failed to load Footer...
+          Failed to load Footer.
         </div>
       </div>
-    </div>
-    <div
-      class="alert alert-success px-3"
-      role="alert"
-      v-if="['workbench'].indexOf($route.name) > -1 && authenticated === true && (UserPolicyCookieDismiss === false || UserPolicyCookieDismiss == undefined)">
-      <div class="col-12 text-center p-2">While we do not share user credentials, <strong>we can use a cookie to remember your login preferences.<br > Would you like us to remember which login option you chose next time?</strong>  <a
-        href="https://www.epa.gov/privacy/privacy-and-security-notice#cookies"
-        target="_blank"
-        class="text-dark text-decoration-underline pl-2">See our cookie policy</a>.</div>
-      <div class="col-12 text-right p-2 w-75"><a
-        href="#"
-        @click="setUserPolicyCookie"
-        data-dismiss="alert"
-        class="text-dark text-decoration-underline pr-2"
-      >No Thanks</a><button
-        class="btn btn-primary"
-        data-dismiss="alert"
-        @click="setUserPolicyCookie">Yes, Please</button></div>
     </div>
   </footer>
 </template>
 <script>
   import AppAxios from 'axios';
-  import { mapGetters, mapActions } from 'vuex';
+  import { mapActions, mapGetters } from 'vuex';
 
   export default {
     name: 'MainFooter',
+    components: { },
     computed: {
       ...mapGetters({
-        authenticated: 'getIsLoggedIn',
-        termsAndConditionsCookie: 'getTermsAndConditionsCookie',
-        UserPolicyCookieDismiss: 'getUserPolicyCookieDismiss',
         apiUrl: 'getEnvironmentApiURL',
       }),
     },
     methods: {
       ...mapActions([
-        'onTermsAndConditions',
-        'setUserPolicyCookie',
       ]),
     },
     data() {
@@ -122,20 +76,19 @@
     },
     mounted() {
       AppAxios
-        .get(`${this.apiUrl}/api/footer`)
-        .then((response) => {
-          this.footerLinks = response.data[0].field_footer_link_name;
-          this.footerVersion = response.data[0].field_version;
-          this.footerLinksLoaded = true;
-        });
+              .get(`${this.apiUrl}/api/footer`)
+              .then((response) => {
+                this.footerLinks = response.data[0].field_footer_link_name;
+                this.footerVersion = response.data[0].field_version;
+                this.footerLinksLoaded = true;
+              });
     },
   };
 
 </script>
 
 <style scoped
-  lang="scss">
-  @import '../styles/bootstrap-mixins-cheatsheet.scss';
+       lang="scss">
   footer {
     color: #fefefe;
     background-color: #323a45;
@@ -149,9 +102,4 @@
     }
   }
 
-  #terms-conditions {
-    min-height: 5em;
-    max-width: none;
-    z-index: 1200;
-  }
 </style>
