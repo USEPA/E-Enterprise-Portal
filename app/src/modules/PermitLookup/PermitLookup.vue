e3e0ed2b633bdb31fb9aba452e143fcffa637f29
mculpepp <michael.culpepper@cgifederal.com>
2019-03-16 12:47:56 -0500

    PERMITS

--- app/src/modules/PermitLookup/PermitLookup.vue
+++ app/src/modules/PermitLookup/PermitLookup.vue
@@ -1003,23 +1003,29 @@
                   :total-rows="totalRows"
                   :per-page="perPage"
                   v-model="currentPage"
-                  class="my-0"/>
                   class="my-0">
-                  <div class="wapp-arrows"
-                    slot="first-text"><img src="/images/pager-first.png"
-                    alt="Go to first page"></div>
-                  <div class="wapp-arrows"
-                    slot="next-text"><img src="/images/pager-next.png"
-                    alt="Go to next page"></div>
-                  <div class="wapp-arrows"
-                    slot="prev-text"><img src="/images/pager-previous.png"
-                    alt="Go to previous page"></div>
-                  <div class="wapp-arrows"
-                    slot="last-text"><img src="/images/pager-last.png"
-                    alt="Go to last page"></div>
+                  <div
+                    class="wapp-arrows"
+                    slot="first-text"><img
+                      src="/images/pager-first.png"
+                      alt="Go to first page"></div>
+                  <div
+                    class="wapp-arrows"
+                    slot="next-text"><img
+                      src="/images/pager-next.png"
+                      alt="Go to next page"></div>
+                  <div
+                    class="wapp-arrows"
+                    slot="prev-text"><img
+                      src="/images/pager-previous.png"
+                      alt="Go to previous page"></div>
+                  <div
+                    class="wapp-arrows"
+                    slot="last-text"><img
+                      src="/images/pager-last.png"
+                      alt="Go to last page"></div>
                 </b-pagination>
               </b-col>
-            </b-row>
             </b-row>
 
           </AppModal>
@@ -1192,7 +1198,7 @@
 <script>
 
   import { mapActions, mapGetters } from 'vuex';
-  import { AppWrapper, AppModal, PaginationArrows } } from '../wadk/WADK';
+  import { AppWrapper, AppModal } from '../wadk/WADK';
   import storeModule from './store/index';
 
   const moduleName = 'PermitLookup';
@@ -1202,7 +1208,6 @@
     components: {
       AppWrapper,
       AppModal,
-      PaginationArrows,
     },
     beforeCreate() {
 
