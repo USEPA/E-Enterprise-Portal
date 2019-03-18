d50a6dc502158b8b28f3b15824404df1b51b9e24
smolinsk <samuel.molinski@cgifederal.com>
2019-03-18 09:34:20 -0500

    - Minor refactoring - Added proxy services

--- app/src/modules/wadk/store/common/getters.js
+++ app/src/modules/wadk/store/common/getters.js
@@ -5,4 +5,17 @@
  *
  * @SEE https://vuex.vuejs.org/guide/getters.html
  */
-export default {};
+export default {
+  getApiUrl: (state, getters, rootState, rootGetters) => (urlName) => {
+    const envApiUrl = rootGetters.getEnvironmentApiURL;
+    let response = `${envApiUrl}/${state.api.urls[urlName]}`;
+    if (state.api.urls[urlName].search(/http[s]?:/) > -1) {
+      response = state.api.urls[urlName];
+    } else if (state.api.urls[urlName].search(/internal:/) > -1) {
+      const { origin } = window.location;
+      const url = state.api.urls[urlName].replace(/internal:/, '');
+      response = `${origin}/${url}`;
+    }
+    return response;
+  },
+};
