71a5069986d8727af3186c7ce4e9019fc7bb7432
RBEACH <richard.h.beach@cgi.com>
2019-03-22 17:20:06 -0400

    added comments on functionality

--- app/src/App.vue
+++ app/src/App.vue
@@ -45,9 +45,7 @@
     </div>
     <!-- issue here with container and the caroseul -->
     <div class="container px-0">
-      <div
-        id="main-content"
-        class="no-gutters py-2">
+      <div id="main-content" class="no-gutters py-2">
         <router-view/>
       </div>
     </div>
