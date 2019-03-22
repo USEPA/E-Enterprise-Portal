fb2e48071296d1aa472eb0651a7ee140112a08f8
hkunatha <himabindu.kunatha@cgifederal.com>
2019-03-22 07:18:33 -0500

    Default Changelist

--- app/src/components/MainFooter.vue
+++ app/src/components/MainFooter.vue
@@ -2,7 +2,7 @@
   <footer
     v-bind:class="getClass()">
     <div
-      v-if="footerLinksLoaded && footerLinks.length > 0"
+      v-if="!displayLoggedInElements && footerLinksLoaded && footerLinks.length > 0"
       class="container py-2">
       <div class="row justify-content-center small">
         <div
@@ -14,6 +14,27 @@
             class="">{{ item.first }}</a>
         </div>
       </div>
+      <div class="row justify-content-center small">
+        <div class="col-auto text-align-center small">
+          {{ this.footerVersion[0].first }} {{ this.footerVersion[0].second }}
+        </div>
+      </div>
+    </div>
+    <div v-else-if="displayLoggedInElements && footerLinksLoaded && footerLinks.length > 0"
+         class="container py-2">
+      <div class="row justify-content-center small">
+        <div
+          class="col-auto"
+          v-for="item in loginFooterLinks" >
+
+          <a
+            :href="item.second"
+            target="_blank"
+            class="">{{ item.first }}</a>
+
+        </div>
+          </div>
+
       <div class="row justify-content-center small">
         <div class="col-auto text-align-center small">
           {{ this.footerVersion[0].first }} {{ this.footerVersion[0].second }}
@@ -62,7 +83,44 @@
     computed: {
       ...mapGetters({
         apiUrl: 'getEnvironmentApiURL',
+        displayLoggedInElements: 'getDisplayLoggedInElements',
       }),
+
+      loginFooterLinks: function () {
+        for (let i = 0; i < this.footerLinks.length; i++) {
+          for (let j = 0; j < this.loginfooterLinks.length; j++) {
+            if(this.filteredFooterLinks.length>0) {
+              if (this.footerLinks[i].first == this.loginfooterLinks[j].first) {
+                this.filteredFooterLinks.push(this.loginfooterLinks[j]);
+
+              }else{
+                this.filteredFooterLinks.push(this.loginfooterLinks[j])
+
+              }
+            }
+
+          }
+          if(this.filteredFooterLinks.length>0) {
+            if (this.filteredFooterLinks[i].first != this.footerLinks[i].first) {
+              this.filteredFooterLinks.push(this.footerLinks[i])
+            }else if (this.filteredFooterLinks[i].first == this.footerLinks[i].first){
+            }
+            else{
+              this.filteredFooterLinks.push(this.footerLinks[i])
+            }
+          }else{
+            this.filteredFooterLinks.push(this.footerLinks[i])
+          }
+        }
+        //removing duplicates
+        let uniqValues =this.filteredFooterLinks.reduce(function(a,b){
+          if (a.indexOf(b) < 0 ) a.push(b);
+          return a;
+        },[]);
+        this.filteredFooterLinks=uniqValues;
+        return this.filteredFooterLinks;
+
+      }
     },
     methods: {
       ...mapActions([
@@ -74,11 +132,14 @@
           return 'footer';
         }
       },
+
     },
     data() {
       return {
         footerLinks: [],
         footerVersion: [],
+        loginfooterLinks: [],
+        filteredFooterLinks:[],
         footerLinksLoaded: false,
       };
     },
@@ -88,8 +149,11 @@
         .then((response) => {
           this.footerLinks = response.data[0].field_footer_link_name;
           this.footerVersion = response.data[0].field_version;
+          this.loginfooterLinks = response.data[0].field_login_footer_links;
           this.footerLinksLoaded = true;
+
         });
+
     },
   };
 
@@ -112,6 +176,7 @@
   footer {
     color: #fefefe;
     background-color: #323a45;
+
 
     a {
       color: #fefefe;
